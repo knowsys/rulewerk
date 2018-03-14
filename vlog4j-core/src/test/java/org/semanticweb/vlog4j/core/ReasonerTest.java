05437647f35b74b340d6932fb389335f9b861e7b
carralma <carralma@thi105.inf.tu-dresden.de>
2018-03-14 16:00:04 +0100

    Renamed ReasonerTest1.java to ReasonerTest.java

--- vlog4j-core/src/test/java/org/semanticweb/vlog4j/core/ReasonerTest.java
+++ vlog4j-core/src/test/java/org/semanticweb/vlog4j/core/ReasonerTest.java
@@ -9,9 +9,9 @@ package org.semanticweb.vlog4j.core;
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *      http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -19,7 +19,6 @@ package org.semanticweb.vlog4j.core;
  * limitations under the License.
  * #L%
  */
-
 
 import java.io.IOException;
 import java.util.ArrayList;
@@ -44,7 +43,7 @@ import karmaresearch.vlog.EDBConfigurationException;
 import karmaresearch.vlog.NotStartedException;
 import karmaresearch.vlog.StringQueryResultEnumeration;
 
-public class ReasonerTest1 extends TestCase {
+public class ReasonerTest extends TestCase {
 
 	public void testSimpleInference() throws VLog4jTermValidationException, VLog4jAtomValidationException, VLog4jRuleValidationException,
 			AlreadyStartedException, EDBConfigurationException, IOException, NotStartedException {
