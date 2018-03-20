2723d903c0243085cead50d1013ec20867535f1d
mkroetzsch <accounts@korrekt.org>
2018-03-20 10:58:23 +0100

    Script for making the VLog jar

---
+++ vlog4j-core/build-vlog-library.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+# Script to build the necessary VLog jar on Unix-like systems
+
+rm -rf build-vlog
+mkdir build-vlog
+cd build-vlog
+git clone https://github.com/karmaresearch/vlog.git
+# cd vlog
+# git pull
+mkdir build
+cd build
+cmake -DJAVA=1 ..
+make
+cp jvlog.jar ../../../lib/jvlog.jar
