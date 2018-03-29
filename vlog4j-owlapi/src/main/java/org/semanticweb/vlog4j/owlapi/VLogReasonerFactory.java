085aae97314f4a123fb2523e3cba8d5cb557c8c8
Irina.Dragoste <Irina.Dragoste@tu-dresden.de>
2018-03-29 14:36:39 +0200

    skeleton of a vLog implementation for OWLReasoner and OWLReasonerFactory

---
+++ vlog4j-owlapi/src/main/java/org/semanticweb/vlog4j/owlapi/VLogReasonerFactory.java
@@ -0,0 +1,40 @@
+package org.semanticweb.vlog4j.owlapi;
+
+import org.semanticweb.owlapi.model.OWLOntology;
+import org.semanticweb.owlapi.reasoner.OWLReasoner;
+import org.semanticweb.owlapi.reasoner.OWLReasonerConfiguration;
+import org.semanticweb.owlapi.reasoner.OWLReasonerFactory;
+
+public class VLogReasonerFactory implements OWLReasonerFactory {
+
+	@Override
+	public OWLReasoner createNonBufferingReasoner(OWLOntology arg0) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public OWLReasoner createNonBufferingReasoner(OWLOntology arg0, OWLReasonerConfiguration arg1) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public OWLReasoner createReasoner(OWLOntology arg0) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public OWLReasoner createReasoner(OWLOntology arg0, OWLReasonerConfiguration arg1) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String getReasonerName() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
