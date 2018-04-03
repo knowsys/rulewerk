c8d76bef65ca633df83d6eb655fb9105be980591
Irina.Dragoste <Irina.Dragoste@tu-dresden.de>
2018-04-03 19:39:20 +0200

    added SPARQL query datasource class

---
+++ vlog4j-core/src/main/java/org/semanticweb/vlog4j/core/reasoner/implementation/SparqlQueryResultDataSource.java
@@ -0,0 +1,13 @@
+package org.semanticweb.vlog4j.core.reasoner.implementation;
+
+import org.semanticweb.vlog4j.core.reasoner.DataSource;
+
+public class SparqlQueryResultDataSource implements DataSource {
+
+	@Override
+	public String toConfigString() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
