08b1b5cc8235021a8c9b514bd94c7fbeda62eb22
David Carral <david.carral@tu-dresden.de>
2018-04-04 22:40:19 +0200

    First commit in the evaluation branch.

---
+++ vlog4j-evaluation/src/main/java/org/semanticweb/vlog4j/evaluation/App.java
@@ -0,0 +1,13 @@
+package org.semanticweb.vlog4j.evaluation;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
