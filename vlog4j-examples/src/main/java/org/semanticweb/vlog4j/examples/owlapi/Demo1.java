684a997d4502ecb97f6078de812ef5f07fe4edcf
David Carral <david.carral@tu-dresden.de>
2018-04-20 11:16:04 +0200

    Added Demo1.java and Demo2.java.

---
+++ vlog4j-examples/src/main/java/org/semanticweb/vlog4j/examples/owlapi/Demo1.java
@@ -0,0 +1,7 @@
+package org.semanticweb.vlog4j.examples.owlapi;
+
+public class Demo1 {
+	public static void main(String[] args) {
+
+	}
+}
---
+++ vlog4j-examples/src/main/java/org/semanticweb/vlog4j/examples/owlapi/Demo2.java
@@ -0,0 +1,5 @@
+package org.semanticweb.vlog4j.examples.owlapi;
+
+public class Demo2 {
+
+}
