b821f76884769a9e9f9808378a5c7da5e22ca17d
Irina Dragoste <irina.dragoste@tu-dresden.de>
2018-05-09 10:52:27 +0200

    rename **RestrictedChaseOnOwlOntology** to **OwlOntologyToRulesAndFacts** for a clearer suggestion of how to transform the OWL ontology to rules and facts.

--- vlog4j-examples/src/main/java/org/semanticweb/vlog4j/examples/owlapi/OwlOntologyToRulesAndFacts.java
+++ vlog4j-examples/src/main/java/org/semanticweb/vlog4j/examples/owlapi/OwlOntologyToRulesAndFacts.java
@@ -32,6 +32,7 @@ import org.semanticweb.owlapi.model.OWLOntologyManager;
 import org.semanticweb.vlog4j.core.model.api.Atom;
 import org.semanticweb.vlog4j.core.model.api.Constant;
 import org.semanticweb.vlog4j.core.model.api.Rule;
+import org.semanticweb.vlog4j.core.model.api.Term;
 import org.semanticweb.vlog4j.core.model.api.Variable;
 import org.semanticweb.vlog4j.core.model.implementation.Expressions;
 import org.semanticweb.vlog4j.core.reasoner.Reasoner;
@@ -42,15 +43,14 @@ import org.semanticweb.vlog4j.core.reasoner.implementation.QueryResultIterator;
 import org.semanticweb.vlog4j.owlapi.OwlToRulesConverter;
 
 /**
- * This example shows how <b>vlog4j-owlapi</b> library (class {@link OwlToRulesConverter}) can be used to transform
- * an OWL ontology into <b>vlog4j-core</b> {@link Rule}s and {@link Atom}s.
+ * This example shows how <b>vlog4j-owlapi</b> library (class
+ * {@link OwlToRulesConverter}) can be used to transform an OWL ontology into
+ * <b>vlog4j-core</b> {@link Rule}s and {@link Atom}s.
  * 
  * @author Irina Dragoste
  *
  */
-public class RestrictedChaseOnOwlOntology {
-
-	// FIXME change www.bike.org name
+public class OwlOntologyToRulesAndFacts {
 
 	public static void main(String[] args) throws OWLOntologyCreationException, ReasonerStateException,
 			EdbIdbSeparationException, IncompatiblePredicateArityException, IOException {
@@ -87,26 +87,58 @@ public class RestrictedChaseOnOwlOntology {
 			reasoner.addRules(new ArrayList<Rule>(owlToRulesConverter.getRules()));
 			reasoner.addFacts(owlToRulesConverter.getFacts());
 			reasoner.load();
-			/* Reason over loaded ontology */
+			
+			/* Reason over loaded ontology with the default algorithm Restricted Chase*/
+			System.out.println("Reasoning default algorithm: "+ reasoner.getAlgorithm());
 			reasoner.reason();
 
 			/* Query for the parts of bike constant "b2". */
 			Variable vx = Expressions.makeVariable("x");
 			Constant b2 = Expressions.makeConstant("http://www.bike-example.ontology#b2");
-			Atom isPartOfPairs = Expressions.makeAtom("http://www.bike-example.ontology#isPartOf", vx, b2);
+
+			Atom b2HasPart = Expressions.makeAtom("http://www.bike-example.ontology#hasPart", b2, vx);
+			System.out.println("Answers to query " + b2HasPart + " :");
 
 			/*
 			 * See that an unnamed individual has been introduced to satisfy
 			 * owl:someValuesFrom restriction:
 			 * 
-			 * http://www.semanticweb.org/carralma/ontologies/2018/3/untitled-ontology-2#
-			 * Bike :Bike rdf:type owl:Class ; rdfs:subClassOf [ rdf:type owl:Restriction ;
-			 * owl:onProperty :hasPart ; owl:someValuesFrom :Wheel ] .
+			 * :Bike rdf:type owl:Class ; 
+			 * 		 rdfs:subClassOf [ rdf:type owl:Restriction ;
+			 * 						   owl:onProperty :hasPart ; 
+			 * 						   owl:someValuesFrom :Wheel 
+			 * 						 ] .
 			 */
-			try (QueryResultIterator answers = reasoner.answerQuery(isPartOfPairs, true);) {
-				answers.forEachRemaining(answer -> System.out
-						.println(answer.getTerms().get(0) + " isPartOf " + answer.getTerms().get(1)));
+			try (QueryResultIterator answers = reasoner.answerQuery(b2HasPart, true);) {
+				answers.forEachRemaining(answer -> {
+					final Term constantB2 = answer.getTerms().get(0);
+					final Term term = answer.getTerms().get(1);
+					System.out.println(" - " + constantB2 + " hasPart " + term);
+					System.out.println("   Term " + term + " is of type " + term.getType());
+				});
 			}
+
+			Atom isPartOfB2 = Expressions.makeAtom("http://www.bike-example.ontology#isPartOf", vx, b2);
+			
+			System.out.println("Answers to query " + isPartOfB2 + " :");
+			/*
+			 * See that the same unnamed individual is part of Bike b2, satisfying restriction
+			 * :Wheel rdf:type owl:Class ;
+       		 * 		  rdfs:subClassOf [ rdf:type owl:Restriction ;
+             *             				owl:onProperty :isPartOf ;
+             *             				owl:someValuesFrom :Bike
+             *           				  ] .
+			 */
+			try (QueryResultIterator answers = reasoner.answerQuery(isPartOfB2, true);) {
+				answers.forEachRemaining(answer -> {
+					Term term = answer.getTerms().get(0);
+					Term constantB2 = answer.getTerms().get(1);
+					System.out
+							.println(" - " + term + " isPartOf " + constantB2);
+					System.out.println("   Term " + term + " is of type " + term.getType());
+				});
+			}
+
 		}
 	}
 }
