610ae3ca45e44ab2b61553d32dd2551a70f2baaf
Christian Lewe <ch_lewe@wh2.tu-dresden.de>
2018-09-16 15:03:08 +0200

    Fixed Javadoc
    Now, running `mvn javadoc:javadoc` shouldn't result in an ERROR anymore.

--- vlog4j-core/src/main/java/org/semanticweb/vlog4j/core/model/implementation/AbstractTermImpl.java
+++ vlog4j-core/src/main/java/org/semanticweb/vlog4j/core/model/implementation/AbstractTermImpl.java
@@ -2,8 +2,11 @@ package org.semanticweb.vlog4j.core.model.implementation;
 
 import org.apache.commons.lang3.Validate;
 import org.eclipse.jdt.annotation.NonNull;
+import org.semanticweb.vlog4j.core.model.api.Blank;
+import org.semanticweb.vlog4j.core.model.api.Constant;
 import org.semanticweb.vlog4j.core.model.api.Term;
 import org.semanticweb.vlog4j.core.model.api.TermType;
+import org.semanticweb.vlog4j.core.model.api.Variable;
 
 /*
  * #%L
@@ -14,9 +17,9 @@ import org.semanticweb.vlog4j.core.model.api.TermType;
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *      http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -27,8 +30,7 @@ import org.semanticweb.vlog4j.core.model.api.TermType;
 
 /**
  * Abstract class implementing all methods used by all types of terms
- * ({@link TermType##CONSTANT}, {@link TermType##BLANK}, and
- * {@link TermType##VARIABLE}).
+ * ({@link Constant}, {@link Blank}, and {@link Variable}).
  *
  * @author david.carral@tu-dresden.de
  */
--- vlog4j-core/src/main/java/org/semanticweb/vlog4j/core/model/implementation/ConstantImpl.java
+++ vlog4j-core/src/main/java/org/semanticweb/vlog4j/core/model/implementation/ConstantImpl.java
@@ -13,9 +13,9 @@ import org.semanticweb.vlog4j.core.model.api.TermVisitor;
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *      http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -25,7 +25,8 @@ import org.semanticweb.vlog4j.core.model.api.TermVisitor;
  */
 
 /**
- * Implements {@link #CONSTANT} terms. A constant is an entity used to represent named domain elements in the domain.
+ * Implements {@link Constant} terms. A constant is an entity used to represent
+ * named domain elements in the domain.
  *
  * @author david.carral@tu-dresden.de
  */
