52d70fa2c70c1dbbb28db05d14285d88170ec049
Christian Lewe <ch_lewe@wh2.tu-dresden.de>
2018-10-30 22:06:50 +0100

    Deprecated and ignored an old RDF test

--- vlog4j-rdf/src/test/java/org/semanticweb/vlog4j/rdf/BlankIdsInDifferentModelsTest.java
+++ vlog4j-rdf/src/test/java/org/semanticweb/vlog4j/rdf/BlankIdsInDifferentModelsTest.java
@@ -9,9 +9,9 @@ package org.semanticweb.vlog4j.rdf;
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *      http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -28,6 +28,7 @@ import java.io.IOException;
 import java.util.HashSet;
 import java.util.Set;
 
+import org.junit.Ignore;
 import org.junit.Test;
 import org.openrdf.model.BNode;
 import org.openrdf.model.Model;
@@ -37,6 +38,8 @@ import org.openrdf.rio.RDFFormat;
 import org.openrdf.rio.RDFHandlerException;
 import org.openrdf.rio.RDFParseException;
 
+@Deprecated
+@Ignore
 public class BlankIdsInDifferentModelsTest {
 
 	@Test
@@ -77,7 +80,7 @@ public class BlankIdsInDifferentModelsTest {
 		assertTrue(intersectionModel2Model3.isEmpty());
 	}
 
-	private Set<String> collectBlankNodeIds(Model model) {
+	private Set<String> collectBlankNodeIds(final Model model) {
 		final HashSet<String> blankNodeIds = new HashSet<>();
 		model.forEach(statement -> {
 			final Resource subject = statement.getSubject();
