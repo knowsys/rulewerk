2e8bbdc05be7b65e8263ace30ded52e5dfe3c697
Christian Lewe <ch_lewe@wh2.tu-dresden.de>
2018-11-10 22:47:14 +0100

    Reworked `TestReasonOverRdfFacts`
    * Reworked example-facts.ttl.
    * Removed unnecessary `NoBlanks` variant.
    * Added test for correct RDF query results.

--- vlog4j-rdf/src/test/java/org/semanticweb/vlog4j/rdf/TestReasonOverRdfFacts.java
+++ vlog4j-rdf/src/test/java/org/semanticweb/vlog4j/rdf/TestReasonOverRdfFacts.java
@@ -1,6 +1,6 @@
 package org.semanticweb.vlog4j.rdf;
 
-import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.assertEquals;
 
 /*-
  * #%L
@@ -22,8 +22,17 @@ import static org.junit.Assert.assertTrue;
  * #L%
  */
 
+import static org.junit.Assert.assertTrue;
+import static org.semanticweb.vlog4j.core.model.implementation.Expressions.makeAtom;
+import static org.semanticweb.vlog4j.core.model.implementation.Expressions.makeConstant;
+import static org.semanticweb.vlog4j.core.model.implementation.Expressions.makeVariable;
+import static org.semanticweb.vlog4j.rdf.RdfModelToAtomsConverter.RDF_TRIPLE_PREDICATE_NAME;
+
 import java.io.File;
 import java.io.IOException;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
 
 import org.junit.Test;
@@ -32,7 +41,9 @@ import org.openrdf.rio.RDFFormat;
 import org.openrdf.rio.RDFHandlerException;
 import org.openrdf.rio.RDFParseException;
 import org.semanticweb.vlog4j.core.model.api.Atom;
-import org.semanticweb.vlog4j.core.model.implementation.Expressions;
+import org.semanticweb.vlog4j.core.model.api.Constant;
+import org.semanticweb.vlog4j.core.model.api.Term;
+import org.semanticweb.vlog4j.core.model.api.Variable;
 import org.semanticweb.vlog4j.core.reasoner.Reasoner;
 import org.semanticweb.vlog4j.core.reasoner.exceptions.EdbIdbSeparationException;
 import org.semanticweb.vlog4j.core.reasoner.exceptions.IncompatiblePredicateArityException;
@@ -41,26 +52,60 @@ import org.semanticweb.vlog4j.core.reasoner.implementation.QueryResultIterator;
 
 public class TestReasonOverRdfFacts {
 
-	// TODO add rules, reason
-	// TODO add data of each type
+	private static final Set<List<Term>> expectedQueryResultsInvention = new HashSet<>(Arrays.asList(
+			Arrays.asList(makeConstant("https://example.org/Carl-Benz"), makeConstant("https://example.org/invention"), makeConstant("\"car\"@en")),
+			Arrays.asList(makeConstant("https://example.org/Carl-Benz"), makeConstant("https://example.org/invention"),
+					makeConstant("\"Auto\"@de"))));
+
+	private static final Variable subject = makeVariable("s");
+	private static final Variable predicate = makeVariable("p");
+	private static final Variable object = makeVariable("o");
 
 	@Test
-	public void testLoadFactsFromRDF() throws RDFParseException, RDFHandlerException, IOException,
+	public void testCanLoadRdfFactsIntoReasoner() throws RDFParseException, RDFHandlerException, IOException,
 	ReasonerStateException, EdbIdbSeparationException, IncompatiblePredicateArityException {
-		final Model model = RdfTestUtils.parseFile(
-				new File(RdfTestUtils.INPUT_FOLDER + "exampleFactsNoBlanks.ttl"), RDFFormat.TURTLE);
+		final Model model = RdfTestUtils.parseFile(new File(RdfTestUtils.INPUT_FOLDER + "exampleFacts.ttl"),
+				RDFFormat.TURTLE);
 		final Set<Atom> facts = RdfModelToAtomsConverter.rdfModelToAtoms(model);
 
 		try (final Reasoner reasoner = Reasoner.getInstance()) {
 			reasoner.addFacts(facts);
 			reasoner.load();
-			final QueryResultIterator answerQuery = reasoner.answerQuery(
-					Expressions.makeAtom(RdfModelToAtomsConverter.RDF_TRIPLE_PREDICATE, Expressions.makeVariable("x"),
-							Expressions.makeVariable("y"), Expressions.makeVariable("z")),
-					true);
-			assertTrue(answerQuery.hasNext());
-			answerQuery.close();
+
+			final Atom universalQuery = makeAtom(RDF_TRIPLE_PREDICATE_NAME, subject, predicate, object);
+			final Set<List<Term>> queryResults = getQueryResults(reasoner, universalQuery);
+			assertTrue(!queryResults.isEmpty());
 		}
+	}
+
+	@Test
+	public void testQueryAnsweringOverRdfFacts() throws RDFParseException, RDFHandlerException, IOException,
+	ReasonerStateException, EdbIdbSeparationException, IncompatiblePredicateArityException {
+		final Model model = RdfTestUtils.parseFile(
+				new File(RdfTestUtils.INPUT_FOLDER + "exampleFacts.ttl"), RDFFormat.TURTLE);
+		final Set<Atom> facts = RdfModelToAtomsConverter.rdfModelToAtoms(model);
+
+		try (final Reasoner reasoner = Reasoner.getInstance()) {
+			reasoner.addFacts(facts);
+			reasoner.load();
+
+			final Constant inventionPredicate = makeConstant("https://example.org/invention");
+			final Constant carlBenzSubject = makeConstant("https://example.org/Carl-Benz");
+
+			final Atom inventionQuery = makeAtom(RDF_TRIPLE_PREDICATE_NAME, carlBenzSubject, inventionPredicate,
+					object);
+			assertEquals(expectedQueryResultsInvention, getQueryResults(reasoner, inventionQuery));
+		}
+	}
+
+	private Set<List<Term>> getQueryResults(final Reasoner reasoner, final Atom queryAtom)
+			throws ReasonerStateException {
+		final QueryResultIterator queryResultIterator = reasoner.answerQuery(queryAtom, true);
+
+		final Set<List<Term>> queryResults = new HashSet<>();
+		queryResultIterator.forEachRemaining(queryResult -> queryResults.add(queryResult.getTerms()));
+		queryResultIterator.close();
+		return queryResults;
 	}
 
 }
