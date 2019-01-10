73a36796ba8e45be7a7b654e4892d7430e746145
Adrian Bielefeldt <Adrian.Bielefeldt@mailbox.tu-dresden.de>
2019-01-10 02:30:54 +0100

    Improved GraalToVLog4JModelConverterTest
    Added more complex atom and query conversion tests.

--- vlog4j-graal/src/test/java/org/semanticweb/vlog4j/graal/GraalToVLog4JModelConverterTest.java
+++ vlog4j-graal/src/test/java/org/semanticweb/vlog4j/graal/GraalToVLog4JModelConverterTest.java
@@ -43,32 +43,42 @@ import fr.lirmm.graphik.graal.io.dlp.DlgpParser;
  */
 public class GraalToVLog4JModelConverterTest {
 	
+	private final String socrate = "socrate";
+	private final String redsBike = "redsBike";
+
 	private final String bicycle = "bicycle";
 	private final String hasPart = "hasPart";
 	private final String human = "human";
 	private final String mortal = "mortal";
-	private final String socrate = "socrate";
 	private final String wheel = "wheel";
 	
 	private final String x = "X";
 	private final String y = "Y";
+	private final String z = "Z";
 	
 	private final Constant vlog4j_socrate = makeConstant(socrate);
+	private final Constant vlog4j_redsBike = makeConstant(redsBike);
 	
 	private final Predicate vlog4j_bicycle = makePredicate(bicycle, 1);
 	private final Predicate vlog4j_hasPart = makePredicate(hasPart, 2);
-	private final Predicate vlog4j_mortal = makePredicate(mortal, 1);
 	private final Predicate vlog4j_human = makePredicate(human, 1);
+	private final Predicate vlog4j_mortal = makePredicate(mortal, 1);
 	private final Predicate vlog4j_wheel = makePredicate(wheel, 1);
 	
 	private final Variable vlog4j_x = makeVariable(x);
 	private final Variable vlog4j_y = makeVariable(y);
+	private final Variable vlog4j_z = makeVariable(z);
 
 	@Test
 	public void testConvertAtom() throws ParseException {
 		final Atom vlog4j_atom = makeAtom(vlog4j_human, vlog4j_socrate);
 		final fr.lirmm.graphik.graal.api.core.Atom graal_atom = DlgpParser.parseAtom(human + "(" + socrate + ").");
 		assertEquals(vlog4j_atom, GraalToVLog4JModelConverter.convertAtom(graal_atom));
+
+		final Atom vlog4j_atom_2 = makeAtom(vlog4j_hasPart, vlog4j_redsBike, vlog4j_x);
+		final fr.lirmm.graphik.graal.api.core.Atom graal_atom_2 = DlgpParser
+				.parseAtom(hasPart + "(" + redsBike + ", " + x + ").");
+		assertEquals(vlog4j_atom_2, GraalToVLog4JModelConverter.convertAtom(graal_atom_2));
 	}
 
 	@Test
@@ -102,5 +112,30 @@ public class GraalToVLog4JModelConverterTest {
 		final GraalConjunctiveQueryToRule importedQuery = GraalToVLog4JModelConverter.convertQuery(mortalQuery, DlgpParser.parseQuery("?(" + x + ") :- " + mortal + "(" + x + ")."));
 		assertEquals(query, importedQuery.getQueryAtom());
 		assertEquals(queryRule, importedQuery.getRule());
+
+		final String complexQuery = "complexQuery";
+		final String predicate1 = "predicate1";
+		final String predicate2 = "predicate2";
+		final String predicate3 = "predicate3";
+		final String predicate4 = "predicate4";
+		final String stockholm = "stockholm";
+
+		final Atom complexQueryAtom = makeAtom(makePredicate(complexQuery, 3), vlog4j_x, vlog4j_x, vlog4j_y);
+
+		final Atom vlog4j_predicate1_atom = makeAtom(makePredicate(predicate1, 1), vlog4j_x);
+		final Atom vlog4j_predicate2_atom = makeAtom(makePredicate(predicate2, 2), vlog4j_y, vlog4j_x);
+		final Atom vlog4j_predicate3_atom = makeAtom(makePredicate(predicate3, 2), vlog4j_y, makeConstant(stockholm));
+		final Atom vlog4j_predicate4_atom = makeAtom(makePredicate(predicate4, 3), vlog4j_x, vlog4j_y, vlog4j_z);
+
+		final Rule complexQueryRule = makeRule(complexQueryAtom, vlog4j_predicate1_atom, vlog4j_predicate2_atom,
+				vlog4j_predicate3_atom, vlog4j_predicate4_atom);
+
+		final GraalConjunctiveQueryToRule importedComplexQuery = GraalToVLog4JModelConverter.convertQuery("query",
+				DlgpParser.parseQuery(
+						"?(" + x + ", " + x + ", " + y + ") :- " + predicate1
+								+ "(" + x + "), " + predicate2 + "(" + y + ", " + x + "), " + predicate3 + "(" + y
+								+ ", " + stockholm + "), " + predicate4 + "(" + x + ", " + y + ", " + z + ")."));
+		assertEquals(complexQueryAtom, importedComplexQuery.getQueryAtom());
+		assertEquals(complexQueryRule, importedComplexQuery.getRule());
 	}
 }
