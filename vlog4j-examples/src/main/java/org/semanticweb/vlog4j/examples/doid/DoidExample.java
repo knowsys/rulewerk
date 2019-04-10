00439a1754fae984a0c9e20288951842c7d75de6
mkroetzsch <accounts@korrekt.org>
2019-04-10 17:58:52 +0200

    use new rules in doid example

--- vlog4j-examples/src/main/java/org/semanticweb/vlog4j/examples/doid/DoidExample.java
+++ vlog4j-examples/src/main/java/org/semanticweb/vlog4j/examples/doid/DoidExample.java
@@ -1,13 +1,35 @@
 package org.semanticweb.vlog4j.examples.doid;
 
+/*-
+ * #%L
+ * VLog4j Examples
+ * %%
+ * Copyright (C) 2018 - 2019 VLog4j Developers
+ * %%
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * #L%
+ */
+
 import static org.semanticweb.vlog4j.core.model.implementation.Expressions.makePredicate;
 import static org.semanticweb.vlog4j.core.model.implementation.Expressions.makeVariable;
 
 import java.io.File;
 import java.io.IOException;
 import java.net.URL;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.LinkedHashSet;
+import java.util.List;
 
 import org.semanticweb.vlog4j.core.model.api.Predicate;
 import org.semanticweb.vlog4j.core.model.api.Variable;
@@ -20,106 +42,71 @@ import org.semanticweb.vlog4j.core.reasoner.exceptions.ReasonerStateException;
 import org.semanticweb.vlog4j.core.reasoner.implementation.RdfFileDataSource;
 import org.semanticweb.vlog4j.core.reasoner.implementation.SparqlQueryResultDataSource;
 import org.semanticweb.vlog4j.examples.ExamplesUtils;
+import org.semanticweb.vlog4j.graal.GraalToVLog4JModelConverter;
+
+import fr.lirmm.graphik.graal.api.core.Rule;
+import fr.lirmm.graphik.graal.io.dlp.DlgpParser;
 
 public class DoidExample {
 	public static void main(String[] args)
 			throws ReasonerStateException, IOException, EdbIdbSeparationException, IncompatiblePredicateArityException {
 
-		final URL wikidataSparqlEndpoint = new URL("https://query.wikidata.org/sparql");
+		/* Configure data sources */
 
 		/* SPARQL queries */
+		final URL wikidataSparqlEndpoint = new URL("https://query.wikidata.org/sparql");
+
+		final String sparqlHumansWithDisease = "?disease wdt:P699 ?doid .";
+		final DataSource diseasesDataSource = new SparqlQueryResultDataSource(wikidataSparqlEndpoint, "disease,doid",
+				sparqlHumansWithDisease);
+
 		final String sparqlRecentDeaths = "?human wdt:P31 wd:Q5; wdt:P570 ?dateofdeath . \n"
 				+ "FILTER (?dateofdeath > \"2018-01-01\"^^xsd:dateTime && ?dateofdeath < \"2019-01-01\"^^xsd:dateTime)";
-		final String sparqlHumansWithDisease = "?human wdt:P31 wd:Q5; wdt:P1050 ?disease .\n"
-				+ "?disease wdt:P31 wd:Q12136; wdt:P2888 ?diseaselinks .";
+
 		final String sparqlRecentDeathsLabel = sparqlRecentDeaths + "?human rdfs:label ?humanLabel .\n"
 				+ "FILTER (lang(?humanLabel) = \"en\")\n";
-		final String sparqlRecentDeathsCause = sparqlRecentDeaths + "?human wdt:P509 ?deadCause . ";
-		final String sparqlRecentDeathsDoid = sparqlRecentDeathsCause + "?deadCause wdt:P699 ?doid . ";
+		final DataSource recentDeathsLabelDataSource = new SparqlQueryResultDataSource(wikidataSparqlEndpoint,
+				"human,humanLabel", sparqlRecentDeathsLabel);
 
-		/* Variables */
-		final Variable humanVar = Expressions.makeVariable("human");
-		final Variable humanLabelVar = Expressions.makeVariable("humanLabel");
-		final Variable deadCauseVar = Expressions.makeVariable("deadCause");
-		final Variable doidVar = Expressions.makeVariable("doid");
+		final String sparqlRecentDeathsCause = sparqlRecentDeaths + "?human wdt:P509 ?causeOfDeath . ";
+		final DataSource recentDeathsCauseDataSource = new SparqlQueryResultDataSource(wikidataSparqlEndpoint,
+				"human,causeOfDeath", sparqlRecentDeathsCause);
 
-		/* Predicates */
-		final Predicate dead = makePredicate("dead", 2); // human humanLabel
-		final Predicate deadCause = makePredicate("deadcause", 2); // human deadCause
-		final Predicate disease = makePredicate("disease", 2); // disease, doid
-
-		/* query variables */
-		final LinkedHashSet<Variable> humanHumanLavelQueryVariables = new LinkedHashSet<>(
-				Arrays.asList(humanVar, humanLabelVar));
-
-		final LinkedHashSet<Variable> humanDeadCauseQueryVariables = new LinkedHashSet<>(
-				Arrays.asList(humanVar, deadCauseVar));
-
-		final LinkedHashSet<Variable> deadCauseDoidQueryVariables = new LinkedHashSet<>(
-				Arrays.asList(deadCauseVar, doidVar));
-
-		/* sparql queries */
-		final DataSource humanHumanLabelDataSource = new SparqlQueryResultDataSource(wikidataSparqlEndpoint,
-				humanHumanLavelQueryVariables, sparqlRecentDeathsLabel);
-
-		final DataSource humanDeadCauseDataSource = new SparqlQueryResultDataSource(wikidataSparqlEndpoint,
-				humanDeadCauseQueryVariables, sparqlRecentDeathsCause);
-
-		final DataSource deadCauseDoidDataSource = new SparqlQueryResultDataSource(wikidataSparqlEndpoint,
-				deadCauseDoidQueryVariables, sparqlRecentDeathsDoid);
-
-		final Predicate triplesEDB = makePredicate("triplesEDB", 3);
-		final Predicate triplesIDB = makePredicate("triplesIDB", 3);
-		final Predicate subClassOfPredicate = makePredicate("subClass", 2);
-
-		final Variable x = makeVariable("x");
-		final Variable y = makeVariable("y");
-		final Variable z = makeVariable("z");
-		final Variable s = makeVariable("s");
-		final Variable p = makeVariable("p");
-		final Variable o = makeVariable("o");
-
-		// final Variable humanVar = Expressions.makeVariable("human");
-		final Variable diseaseVar = Expressions.makeVariable("disease");
-		final Variable diseaseDoidVar = Expressions.makeVariable("diseaselinks");
-		// final Variable deadCause = Expressions.makeVariable("deadCause");
+//		final String sparqlRecentDeathsDoid = sparqlRecentDeathsCause + "?causeOfDeath wdt:P699 ?doid . ";
+//		final DataSource recentDeathsDoidDataSource = new SparqlQueryResultDataSource(wikidataSparqlEndpoint,
+//				"human,causeOfDeath,doid", sparqlRecentDeathsDoid);
 
 		try (final Reasoner reasoner = Reasoner.getInstance()) {
+			// final Predicate predicateTE = Expressions.makePredicate("te", 3);
+			// reasoner.addFactsFromDataSource(predicateTE, rdfFileDataSource);
 
-			// TO DO: import rules from Graal
-			// reasoner.addRules(rule1, rule6);
+			final Predicate diseaseIdPredicate = Expressions.makePredicate("diseaseId", 2);
+			reasoner.addFactsFromDataSource(diseaseIdPredicate, diseasesDataSource);
+			final Predicate recentDeathsLabelPredicate = Expressions.makePredicate("recentDeathsLabel", 2);
+			reasoner.addFactsFromDataSource(recentDeathsLabelPredicate, recentDeathsLabelDataSource);
+			final Predicate recentDeathsCausePredicate = Expressions.makePredicate("recentDeathsCause", 2);
+			reasoner.addFactsFromDataSource(recentDeathsCausePredicate, recentDeathsCauseDataSource);
+//			final Predicate recentDeathsDoidPredicate = Expressions.makePredicate("recentDeathsDoid", 3);
+//			reasoner.addFactsFromDataSource(recentDeathsDoidPredicate, recentDeathsDoidDataSource);
 
-			/* Importing {@code .nt.gz} file as data source. */
-			final DataSource triplesEDBDataSource = new RdfFileDataSource(
-					new File(ExamplesUtils.INPUT_FOLDER + "doid.nt.gz"));
-
-			/* Importing from Sparql */
-
-			final LinkedHashSet<Variable> queryVariables = new LinkedHashSet<>(
-					Arrays.asList(humanVar, diseaseVar, diseaseDoidVar));
-
-			// TO DO: link other data sources
-			final DataSource sparqlQueryResultDataSource = new SparqlQueryResultDataSource(wikidataSparqlEndpoint,
-					queryVariables, sparqlHumansWithDisease);
-
-			reasoner.addFactsFromDataSource(triplesEDB, triplesEDBDataSource);
-			// reasoner.addFactsFromDataSource(livingHumansWithDiseases,
-			// sparqlQueryResultDataSource);
+			final List<Rule> graalRules = new ArrayList<>();
+			try (final DlgpParser parser = new DlgpParser(
+					new File(ExamplesUtils.INPUT_FOLDER + "/graal", "doid-example.dlgp"))) {
+				while (parser.hasNext()) {
+					final Object object = parser.next();
+					if (object instanceof Rule) {
+						graalRules.add((Rule) object);
+					}
+				}
+			}
+			reasoner.addRules(GraalToVLog4JModelConverter.convertRules(graalRules));
 
 			reasoner.load();
-
-			System.out.println("Before materialisation:");
-			/* triplesEDB(?s, <~/hasPart>, ?o) */
-
-			/* The reasoner will use the Restricted Chase by default. */
 			reasoner.reason();
 
-			System.out.println("After materialisation:");
-
-			// reasoner.exportQueryAnswersToCsv(subClassXY, ExamplesUtils.OUTPUT_FOLDER +
-			// "closureSubClass.csv", false);
-			// reasoner.exportQueryAnswersToCsv(humanAtom, ExamplesUtils.OUTPUT_FOLDER +
-			// "humanWithDiseases.csv", false);
+			// TODO query
+			// TODO modify CSV file to contain lowercase inCountry predicate
 		}
+
 	}
 }
