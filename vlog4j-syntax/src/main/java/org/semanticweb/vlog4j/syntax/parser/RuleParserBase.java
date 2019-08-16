3891845384768c47ce94748b9c23cfe521c65331
Larry Gonzalez <larryjgonzalez@gmail.com>
2019-08-16 10:46:13 +0200

    remove uncalled functions

--- vlog4j-syntax/src/main/java/org/semanticweb/vlog4j/syntax/parser/RuleParserBase.java
+++ vlog4j-syntax/src/main/java/org/semanticweb/vlog4j/syntax/parser/RuleParserBase.java
@@ -70,10 +70,6 @@ public class RuleParserBase {
 		return Expressions.makeConstant(lexicalForm + "^^<" + PrefixDeclarations.XSD_DOUBLE + ">");
 	}
 
-	protected static String unescapeStr(String s) throws ParseException {
-		return unescape(s, '\\', false, 1, 1);
-	}
-
 	protected static String unescapeStr(String s, int line, int column) throws ParseException {
 		return unescape(s, '\\', false, line, column);
 	}
@@ -114,41 +110,6 @@ public class RuleParserBase {
 			column = column + 1;
 			i = i + 1;
 
-			// \\u and \\U
-			if (ch2 == 'u') {
-				// i points to the \ so i+6 is next character
-				if (i + 4 >= s.length())
-					throw new ParseException("\\u escape too short, line:" + line + ", column: " + column);
-				int x = hex(s, i + 1, 4, line, column);
-				sb.append((char) x);
-				// Jump 1 2 3 4 -- already skipped \ and u
-				i = i + 4;
-				column = column + 4;
-				continue;
-			}
-			if (ch2 == 'U') {
-				// i points to the \ so i+6 is next character
-				if (i + 8 >= s.length())
-					throw new ParseException("\\U escape too short, line:" + line + ", column: " + column);
-				int x = hex(s, i + 1, 8, line, column);
-				// Convert to UTF-16 codepoint pair.
-				sb.append((char) x);
-				// Jump 1 2 3 4 5 6 7 8 -- already skipped \ and u
-				i = i + 8;
-				column = column + 8;
-				continue;
-			}
-
-			// Are we doing just point code escapes?
-			// If so, \X-anything else is legal as a literal "\" and "X"
-
-			if (pointCodeOnly) {
-				sb.append('\\');
-				sb.append(ch2);
-				i = i + 1;
-				continue;
-			}
-
 			// Not just codepoints. Must be a legal escape.
 			char ch3 = 0;
 			switch (ch2) {
@@ -184,80 +145,6 @@ public class RuleParserBase {
 		return sb.toString();
 	}
 
-	// Line and column that started the escape
-	protected static int hex(String s, int i, int len, int line, int column) throws ParseException {
-//        if ( i+len >= s.length() )
-//        {
-//            
-//        }
-		int x = 0;
-		for (int j = i; j < i + len; j++) {
-			char ch = s.charAt(j);
-			column++;
-			int k = 0;
-			switch (ch) {
-			case '0':
-				k = 0;
-				break;
-			case '1':
-				k = 1;
-				break;
-			case '2':
-				k = 2;
-				break;
-			case '3':
-				k = 3;
-				break;
-			case '4':
-				k = 4;
-				break;
-			case '5':
-				k = 5;
-				break;
-			case '6':
-				k = 6;
-				break;
-			case '7':
-				k = 7;
-				break;
-			case '8':
-				k = 8;
-				break;
-			case '9':
-				k = 9;
-				break;
-			case 'A':
-			case 'a':
-				k = 10;
-				break;
-			case 'B':
-			case 'b':
-				k = 11;
-				break;
-			case 'C':
-			case 'c':
-				k = 12;
-				break;
-			case 'D':
-			case 'd':
-				k = 13;
-				break;
-			case 'E':
-			case 'e':
-				k = 14;
-				break;
-			case 'F':
-			case 'f':
-				k = 15;
-				break;
-			default:
-				throw new ParseException("Illegal hex escape: " + ch + ", line:" + line + ", column: " + column);
-			}
-			x = (x << 4) + k;
-		}
-		return x;
-	}
-
 	/** Remove first and last characters (e.g. ' or "") from a string */
 	protected static String stripQuotes(String s) {
 		return s.substring(1, s.length() - 1);
@@ -288,73 +175,6 @@ public class RuleParserBase {
 		return ret + "^^<http://www.w3.org/2001/XMLSchema#string>";
 	}
 
-	protected static String unescapePName(String s, int line, int column) throws ParseException {
-		char escape = '\\';
-		int idx = s.indexOf(escape);
-
-		if (idx == -1)
-			return s;
-
-		int len = s.length();
-		StringBuilder sb = new StringBuilder();
-
-		for (int i = 0; i < len; i++) {
-			// Copied form unescape abobve - share!
-			char ch = s.charAt(i);
-			// Keep line and column numbers.
-			switch (ch) {
-			case '\n':
-			case '\r':
-				line++;
-				column = 1;
-				break;
-			default:
-				column++;
-				break;
-			}
-
-			if (ch != escape) {
-				sb.append(ch);
-				continue;
-			}
-
-			// Escape
-			if (i >= s.length() - 1)
-				throw new ParseException("Illegal escape at end of string, line:" + line + ", column: " + column);
-			char ch2 = s.charAt(i + 1);
-			column = column + 1;
-			i = i + 1;
-
-			switch (ch2) {
-			case '~':
-			case '.':
-			case '-':
-			case '!':
-			case '$':
-			case '&':
-			case '\'':
-			case '(':
-			case ')':
-			case '*':
-			case '+':
-			case ',':
-			case ';':
-			case '=':
-			case ':':
-			case '/':
-			case '?':
-			case '#':
-			case '@':
-			case '%':
-				sb.append(ch2);
-				break;
-			default:
-				throw new ParseException(
-						"Illegal prefix name escape: " + ch2 + ", line:" + line + ", column: " + column);
-			}
-		}
-		return sb.toString();
-	}
 
 	public List<Rule> getRules() {
 		return rules;
@@ -362,10 +182,6 @@ public class RuleParserBase {
 
 	public List<PositiveLiteral> getFacts() {
 		return facts;
-	}
-
-	public List<PositiveLiteral> getQueries() {
-		return queries;
 	}
 
 }
