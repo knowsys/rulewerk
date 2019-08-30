ca09e62b560adcecad239f1e707f31980f4573a0
Larry Gonzalez <larryjgonzalez@gmail.com>
2019-08-30 16:47:44 +0200

    remove import static

--- vlog4j-core/src/test/java/org/semanticweb/vlog4j/core/reasoner/implementation/RdfFileDataSourceTest.java
+++ vlog4j-core/src/test/java/org/semanticweb/vlog4j/core/reasoner/implementation/RdfFileDataSourceTest.java
@@ -22,12 +22,11 @@ import static org.junit.Assert.assertEquals;
  * #L%
  */
 
-import static org.semanticweb.vlog4j.core.reasoner.implementation.FileDataSourceTestUtils.INPUT_FOLDER;
-
 import java.io.File;
 import java.io.IOException;
 
 import org.junit.Test;
+import org.semanticweb.vlog4j.core.reasoner.implementation.FileDataSourceTestUtils;
 
 public class RdfFileDataSourceTest {
 
@@ -38,29 +37,29 @@ public class RdfFileDataSourceTest {
 
 	@Test(expected = IllegalArgumentException.class)
 	public void testConstructorFalseExtension() throws IOException {
-		new RdfFileDataSource(new File(INPUT_FOLDER + "file.csv"));
+		new RdfFileDataSource(new File(FileDataSourceTestUtils.INPUT_FOLDER + "file.csv"));
 	}
 
 	@Test
 	public void testConstructor() throws IOException {
-		final File unzippedRdfFile = new File(INPUT_FOLDER + "file.nt");
-		final File zippedRdfFile = new File(INPUT_FOLDER + "file.nt.gz");
-		final String dirCanonicalPath = new File(INPUT_FOLDER).getCanonicalPath();
+		final File unzippedRdfFile = new File(FileDataSourceTestUtils.INPUT_FOLDER + "file.nt");
+		final File zippedRdfFile = new File(FileDataSourceTestUtils.INPUT_FOLDER + "file.nt.gz");
+		final String dirCanonicalPath = new File(FileDataSourceTestUtils.INPUT_FOLDER).getCanonicalPath();
 		final RdfFileDataSource unzippedRdfFileDataSource = new RdfFileDataSource(unzippedRdfFile);
 		final RdfFileDataSource zippedRdfFileDataSource = new RdfFileDataSource(zippedRdfFile);
 
-		FileDataSourceTestUtils.testConstructor(unzippedRdfFileDataSource, unzippedRdfFile, dirCanonicalPath,
-				"file");
-		FileDataSourceTestUtils.testConstructor(zippedRdfFileDataSource, zippedRdfFile, dirCanonicalPath,
-				"file");
+		FileDataSourceTestUtils.testConstructor(unzippedRdfFileDataSource, unzippedRdfFile, dirCanonicalPath, "file");
+		FileDataSourceTestUtils.testConstructor(zippedRdfFileDataSource, zippedRdfFile, dirCanonicalPath, "file");
 	}
 
 	@Test
 	public void testToConfigString() throws IOException {
-		final RdfFileDataSource unzippedRdfFileDataSource = new RdfFileDataSource(new File(INPUT_FOLDER + "file.nt"));
-		final RdfFileDataSource zippedRdfFileDataSource = new RdfFileDataSource(new File(INPUT_FOLDER + "file.nt.gz"));
+		final RdfFileDataSource unzippedRdfFileDataSource = new RdfFileDataSource(
+				new File(FileDataSourceTestUtils.INPUT_FOLDER + "file.nt"));
+		final RdfFileDataSource zippedRdfFileDataSource = new RdfFileDataSource(
+				new File(FileDataSourceTestUtils.INPUT_FOLDER + "file.nt.gz"));
 
-		final String expectedDirCanonicalPath = new File(INPUT_FOLDER).getCanonicalPath();
+		final String expectedDirCanonicalPath = new File(FileDataSourceTestUtils.INPUT_FOLDER).getCanonicalPath();
 		final String expectedConfigString = "EDB%1$d_predname=%2$s\n" + "EDB%1$d_type=INMEMORY\n" + "EDB%1$d_param0="
 				+ expectedDirCanonicalPath + "\n" + "EDB%1$d_param1=file\n";
 
