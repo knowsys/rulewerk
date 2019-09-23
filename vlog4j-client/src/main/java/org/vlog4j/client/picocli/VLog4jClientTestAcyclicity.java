3a84706b97da5d4b0599eac80fa5677eabaa2932
Larry Gonzalez <larryjgonzalez@gmail.com>
2019-09-23 17:08:09 +0200

    add todos

--- vlog4j-client/src/main/java/org/vlog4j/client/picocli/VLog4jClientTestAcyclicity.java
+++ vlog4j-client/src/main/java/org/vlog4j/client/picocli/VLog4jClientTestAcyclicity.java
@@ -2,12 +2,14 @@ package org.vlog4j.client.picocli;
 
 import picocli.CommandLine.Command;
 
-@Command(name = "testacyclicity", description = "Test if the rule set satisfy any acyclicity notion")
+@Command(name = "testacyclicity", description = "Test if the rule set satisfies any acyclicity notion")
 public class VLog4jClientTestAcyclicity implements Runnable {
 
+//  TODO implement the following method
 //	@Option(names = "--acyclicity-notion", required = false, description = "Acyclicity notion. One of:JA (Joint Acyclicity), RJA (Restricted Joint Acyclicity), RFA (Model-Faithful Acyclicity), RMFA (Restricted Model-Faithful Acyclicity). All by default.")
 //	String acyclicityNotion;
-//
+
+//  TODO implement the following method
 //	@Option(names = "--rule-file", description = "Rule file in rls syntax", required = true)
 //	private String rulePath;
 
