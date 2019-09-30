125ff0f97cd69fd0269375fdbdd98402e2db483a
Larry Gonzalez <larryjgonzalez@gmail.com>
2019-09-30 10:33:25 +0200

    rename variable *folder to *directory

--- vlog4j-client/src/main/java/org/vlog4j/client/picocli/SaveModel.java
+++ vlog4j-client/src/main/java/org/vlog4j/client/picocli/SaveModel.java
@@ -6,15 +6,15 @@ import picocli.CommandLine.Option;
 
 class SaveModel {
 
-	@Option(names = "--save-model", description = "Boolean. If true, Vlog4jClient will save the model into --output-model-folder. False by default.")
+	@Option(names = "--save-model", description = "Boolean. If true, Vlog4jClient will save the model into --output-model-directory. False by default.")
 	public boolean saveModel = false;
 
-	@Option(names = "--output-model-folder", description = "Folder to store the model. Used only if --store-model is set true. \"model\" by default.")
-	public String outputModelFolder = "model";
+	@Option(names = "--output-model-directory", description = "Directory to store the model. Used only if --store-model is set true. \"model\" by default.")
+	public String outputModelDirectory = "model";
 
 	public boolean check() {
-		if (saveModel & outputModelFolder == null) {
-			System.out.println("--save-model requires an --output-model-folder.");
+		if (saveModel & outputModelDirectory == null) {
+			System.out.println("--save-model requires an --output-model-directory.");
 			return false;
 		} else {
 			return true;
@@ -23,14 +23,14 @@ class SaveModel {
 
 	public void prepare() {
 		if (saveModel) {
-			new File(outputModelFolder).mkdirs();
+			new File(outputModelDirectory).mkdirs();
 		}
 	}
 
 	public void print() {
 		if (saveModel) {
 			System.out.println("  --save-model: " + saveModel);
-			System.out.println("  --output-model-folder: " + outputModelFolder);
+			System.out.println("  --output-model-directory: " + outputModelDirectory);
 		}
 	}
 
--- vlog4j-client/src/main/java/org/vlog4j/client/picocli/SaveQueryResult.java
+++ vlog4j-client/src/main/java/org/vlog4j/client/picocli/SaveQueryResult.java
@@ -6,15 +6,15 @@ import picocli.CommandLine.Option;
 
 class SaveQueryResult {
 
-	@Option(names = "--save-query-results", description = "Boolean. If true, Vlog4jClient will save the query result into --output-query-result-folder. False by default.")
+	@Option(names = "--save-query-results", description = "Boolean. If true, Vlog4jClient will save the query result into --output-query-result-directory. False by default.")
 	public static boolean saveQueryResults = false;
 
-	@Option(names = "--output-query-result-folder", description = "Folder to store the model. Used only if --save-query-results is true. \"query-results\" by default.")
-	public static String outputQueryResultFolder = "query-results";
+	@Option(names = "--output-query-result-directory", description = "Directory to store the model. Used only if --save-query-results is true. \"query-results\" by default.")
+	public static String outputQueryResultDirectory = "query-results";
 
 	public boolean checkConfiguration() {
-		if (saveQueryResults & outputQueryResultFolder == null) {
-			System.out.println("--save-query-results requires an --output-query-result-folder.");
+		if (saveQueryResults & outputQueryResultDirectory == null) {
+			System.out.println("--save-query-results requires an --output-query-result-directory.");
 			return false;
 		} else {
 			return true;
@@ -22,15 +22,16 @@ class SaveQueryResult {
 	}
 
 	public void prepare() {
-		if (saveQueryResults)
-			new File(outputQueryResultFolder).mkdirs();
+		if (saveQueryResults) {
+			new File(outputQueryResultDirectory).mkdirs();
+		}
 	}
 
 	public void print() {
 		if (saveQueryResults) {
 			System.out.println("  --save-query-results: " + saveQueryResults);
-			System.out.println("  --output-query-result-folder: " + outputQueryResultFolder);
+			System.out.println("  --output-query-result-directory: " + outputQueryResultDirectory);
 		}
 	}
-	
+
 }
