262dbfeba720abdd1286ca53083962f32e4bc800
Larry Gonzalez <larryjgonzalez@gmail.com>
2019-09-30 10:57:49 +0200

    add javadoc

--- vlog4j-client/src/main/java/org/vlog4j/client/picocli/PrintQueryResult.java
+++ vlog4j-client/src/main/java/org/vlog4j/client/picocli/PrintQueryResult.java
@@ -3,10 +3,19 @@ package org.vlog4j.client.picocli;
 import picocli.CommandLine.Option;
 
 class PrintQueryResults {
-	/* Store and/or print the query result */
-	@Option(names = "--print-query-result-size", description = "Boolean. If true, Vlog4jClient will print only the size of the query result. True by default.")
+
+	/**
+	 * Boolean. If it is set to true, Vlog4jClient will print the size of the query
+	 * result. True by default. Mutually exclusive with
+	 * {@code --print-complete-query-result}
+	 */
+	@Option(names = "--print-query-result-size", description = "Boolean. If true, Vlog4jClient will print the size of the query result. True by default.")
 	public boolean sizeOnly = true;
 
+	/**
+	 * Boolean. If true, Vlog4jClient will print the query result in stdout. False
+	 * by default. Mutually exclusive with {@code --print-query-result-size}
+	 */
 	@Option(names = "--print-complete-query-result", description = "Boolean. If true, Vlog4jClient will print the query result in stdout. False by default.")
 	public boolean complete = false;
 
