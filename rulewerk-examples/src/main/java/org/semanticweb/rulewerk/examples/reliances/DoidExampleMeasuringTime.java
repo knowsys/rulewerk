8afb6f756478ecb3e4677e565d8d5f5a49016f60
Larry Gonzalez <larryjgonzalez@gmail.com>
2020-09-29 15:26:51 +0200

    ordering input data files

--- rulewerk-examples/src/main/java/org/semanticweb/rulewerk/examples/reliances/DoidExampleMeasuringTime.java
+++ rulewerk-examples/src/main/java/org/semanticweb/rulewerk/examples/reliances/DoidExampleMeasuringTime.java
@@ -1,4 +1,4 @@
-package org.semanticweb.rulewerk.examples;
+package org.semanticweb.rulewerk.examples.reliances;
 
 /*-
  * #%L
@@ -28,6 +28,7 @@ import java.util.List;
 import org.semanticweb.rulewerk.core.reasoner.KnowledgeBase;
 import org.semanticweb.rulewerk.core.reasoner.LogLevel;
 import org.semanticweb.rulewerk.core.reasoner.Reasoner;
+import org.semanticweb.rulewerk.examples.ExamplesUtils;
 import org.semanticweb.rulewerk.reasoner.vlog.VLogReasoner;
 import org.semanticweb.rulewerk.parser.ParsingException;
 import org.semanticweb.rulewerk.parser.RuleParser;
