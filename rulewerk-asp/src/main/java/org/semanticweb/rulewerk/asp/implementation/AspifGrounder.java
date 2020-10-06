1e8c04db055fb63d3fb6d861daa501beb13f2979
Philipp Hanisch <philipp.hanisch1@tu-dresden.de>
2020-10-06 13:49:27 +0200

    [asp] ground (conjunctive) rules

--- rulewerk-asp/src/main/java/org/semanticweb/rulewerk/asp/implementation/AspifGrounder.java
+++ rulewerk-asp/src/main/java/org/semanticweb/rulewerk/asp/implementation/AspifGrounder.java
@@ -25,12 +25,14 @@ import it.unimi.dsi.fastutil.objects.Object2IntOpenHashMap;
 import org.apache.commons.lang3.Validate;
 import org.semanticweb.rulewerk.asp.model.Grounder;
 import org.semanticweb.rulewerk.core.model.api.*;
+import org.semanticweb.rulewerk.core.model.implementation.Expressions;
 import org.semanticweb.rulewerk.core.reasoner.KnowledgeBase;
+import org.semanticweb.rulewerk.core.reasoner.QueryResultIterator;
 import org.semanticweb.rulewerk.core.reasoner.Reasoner;
 
 import java.io.BufferedWriter;
 import java.io.IOException;
-import java.util.Map;
+import java.util.*;
 
 /**
  * An ASP grounder whose output is the aspif format, which is used by Gringo.
@@ -39,8 +41,8 @@ import java.util.Map;
  */
 public class AspifGrounder implements Grounder {
 
-	private final Map<Integer, Literal> integerLiteralMap;
-	private final Map<Literal, Integer> literalIntegerMap;
+	private final Map<Integer, AspifIdentifier> integerAspifIdentifierMap;
+	private final Map<AspifIdentifier, Integer> aspifIdentifierIntegerMap;
 	private int aspifCounter;
 
 	private final KnowledgeBase knowledgeBase;
@@ -62,13 +64,87 @@ public class AspifGrounder implements Grounder {
 		this.knowledgeBase = knowledgeBase;
 		this.reasoner = reasoner;
 		this.writer = writer;
-		this.integerLiteralMap = new Int2ObjectOpenHashMap<>();
-		this.literalIntegerMap = new Object2IntOpenHashMap<>();
+		this.integerAspifIdentifierMap = new Int2ObjectOpenHashMap<>();
+		this.aspifIdentifierIntegerMap = new Object2IntOpenHashMap<>();
 		this.aspifCounter = 1;
+	}
+
+	/**
+	 * Auxiliary class which is used as light-weight collection of elements that uniquely identifies (grounded) literals for
+	 * aspif groundings. Based on these identifiers, the class provides statically the functionality to get an integer
+	 * that is on-the-fly uniquely connected with a certain aspif identifier.
+	 */
+	static class AspifIdentifier {
+
+		final private String predicateName;
+		final private String[] termNames;
+
+		final private List<Term> terms;
+		final private Predicate predicate;
+
+		/**
+		 * Constructor. Create an aspif identifier for the given literal and the list of terms as its arguments.
+		 *
+		 * @param literal     the literal
+		 * @param answerTerms the arguments
+		 */
+		public AspifIdentifier(Literal literal, List<Term> answerTerms) {
+			predicate = literal.getPredicate();
+			predicateName = predicate.getName();
+
+			terms = answerTerms;
+			termNames = new String[answerTerms.size()];
+			int i = 0;
+			for (Term term : answerTerms) {
+				termNames[i++] = term.getName();
+			}
+		}
+
+		public String[] getTermNames() {
+			return termNames;
+		}
+
+		public String getPredicateName() {
+			return predicateName;
+		}
+
+		public List<Term> getTerms() {
+			return terms;
+		}
+
+		public Predicate getPredicate() {
+			return predicate;
+		}
+
+		@Override
+		public int hashCode() {
+			final int prime = 31;
+			int hashcode = 23;
+			hashcode = hashcode * prime + this.predicateName.hashCode();
+			hashcode = hashcode * prime + Arrays.hashCode(termNames);
+			return hashcode;
+		}
+
+		@Override
+		public boolean equals(Object obj) {
+			if (this == obj) {
+				return true;
+			}
+			if (obj == null) {
+				return false;
+			}
+			if (!(obj instanceof AspifIdentifier)) {
+				return false;
+			}
+			final AspifIdentifier other = (AspifIdentifier) obj;
+			return this.predicateName.equals(other.getPredicateName())
+				&& Arrays.equals(this.termNames, other.getTermNames());
+		}
 	}
 
 	@Override
 	public boolean ground() throws IOException {
+		this.reasoner.reason();
 		writer.write("asp 1 0 0");
 		writer.newLine();
 
@@ -79,6 +155,15 @@ public class AspifGrounder implements Grounder {
 			}
 		}
 
+		for (Integer aspifValue : integerAspifIdentifierMap.keySet()) {
+			// We encode a literal in the answer set by its aspif integer, and we transform it back with the help of the
+			// integer-to-literal map later.
+			writer.write("4 "
+				+ aspifValue.toString().length() + " " + aspifValue.toString()
+				+ " 1 " + aspifValue.toString());
+			writer.newLine();
+		}
+
 		writer.write("0");
 		writer.newLine();
 		return true;
@@ -86,13 +171,18 @@ public class AspifGrounder implements Grounder {
 
 	@Override
 	public Map<Integer, Literal> getIntegerLiteralMap() {
-		return this.integerLiteralMap;
+		Map<Integer, Literal> map = new Int2ObjectOpenHashMap<>();
+		for (Integer integer : integerAspifIdentifierMap.keySet()) {
+			AspifIdentifier aspifIdentifier = integerAspifIdentifierMap.get(integer);
+			map.put(integer, Expressions.makePositiveLiteral(aspifIdentifier.getPredicate(), aspifIdentifier.getTerms()));
+		}
+		return map;
 	}
 
 	@Override
 	public Boolean visit(Fact statement) {
 		try {
-			writer.write("1 0 1 " + getAspifValue(statement) + " 0 0");
+			writer.write("1 0 1 " + getAspifValue(statement, statement.getArguments()) + " 0 0");
 			writer.newLine();
 		} catch (IOException ioException) {
 			ioException.printStackTrace();
@@ -103,7 +193,43 @@ public class AspifGrounder implements Grounder {
 
 	@Override
 	public Boolean visit(Rule statement) {
-		return null;
+		PositiveLiteral query = statement.getBodyVariablesLiteral();
+		QueryResultIterator answers = reasoner.answerQuery(query, false);
+		Map<Variable, Term> answerMap = new HashMap<>();
+		try {
+			while (answers.hasNext()) {
+				List<Term> answerTerms = answers.next().getTerms();
+				for (int i=0; i<query.getArguments().size(); i++) {
+					Term queryTerm = query.getArguments().get(i);
+					if (queryTerm.isVariable()) {
+						answerMap.put((Variable) queryTerm, answerTerms.get(i));
+					}
+				}
+
+				// Create the String for the rule body
+				StringBuilder bodyBuilder = new StringBuilder();
+				int countBodyLiterals = 0;
+				for (Literal literal : statement.getBody()) {
+					bodyBuilder.append(" ");
+					bodyBuilder.append(getAspifValue(literal, getGroundTerms(literal, answerMap)));
+					countBodyLiterals++;
+				}
+
+				// As aspif supports only disjunctive rules, we have to write one rule for each head literal
+				for (Literal literal : statement.getHead()) {
+					writer.write("1 0 1 " + getAspifValue(literal, getGroundTerms(literal, answerMap))
+						+ " 0 " + countBodyLiterals + bodyBuilder.toString()
+					);
+					writer.newLine();
+				}
+
+				answerMap.clear();
+			}
+		} catch (IOException ioException) {
+			ioException.printStackTrace();
+			return false;
+		}
+		return true;
 	}
 
 	@Override
@@ -112,27 +238,23 @@ public class AspifGrounder implements Grounder {
 	}
 
 	/**
-	 * Get and possibly negate the aspif integer for a ground literal.
+	 * Get and possibly negate the aspif integer for a literal w.r.t. an answer.
 	 *
-	 * @param literal the aspif identifier for a grounded literal
-	 * @return the aspif integer
+	 * @param literal	  the literal
+	 * @param answerTerms a map representing the answer
+	 * @return			  the aspif value
+	 * @throws IOException an IO exception
 	 */
-	private int getAspifValue(Literal groundLiteral) throws IOException {
-		Integer aspifValue = literalIntegerMap.getOrDefault(groundLiteral, 0);
+	private int getAspifValue(Literal literal, List<Term> answerTerms) throws IOException {
+		AspifIdentifier aspifIdentifier = new AspifIdentifier(literal, answerTerms);
+		Integer aspifValue = aspifIdentifierIntegerMap.getOrDefault(aspifIdentifier, 0);
 		if (aspifValue == 0) {
 			aspifValue = aspifCounter++;
-			literalIntegerMap.put(groundLiteral, aspifValue);
-			integerLiteralMap.put(aspifValue, groundLiteral);
-
-			// We encode a literal in the answer set by its aspif integer, and we transform it back with the help of the
-			// integer-to-literal map later.
-			writer.write("4 "
-				+ aspifValue.toString().length() + " " + aspifValue.toString()
-				+ " 1 " + aspifValue.toString());
-			writer.newLine();
+			aspifIdentifierIntegerMap.put(aspifIdentifier, aspifValue);
+			integerAspifIdentifierMap.put(aspifValue, aspifIdentifier);
 		}
 
-		return groundLiteral.isNegated() ? -aspifValue : aspifValue;
+		return literal.isNegated() ? -aspifValue : aspifValue;
 	}
 
 	/**
@@ -142,5 +264,21 @@ public class AspifGrounder implements Grounder {
 	 */
 	private int getAspifValue() {
 		return aspifCounter++;
+	}
+
+	/**
+	 * Get the ground terms of a literal with respect to a specific answer.
+	 *
+	 * @param literal   the literal to ground
+	 * @param answerMap a map representing the answer
+	 * @return			list of ground terms
+	 */
+	private List<Term> getGroundTerms(Literal literal, Map<Variable, Term> answerMap) {
+		List<Term> groundTerms = new ArrayList<>();
+		for (int i = 0; i < literal.getArguments().size(); i++) {
+			Term term = literal.getArguments().get(i);
+			groundTerms.add(term.isVariable() ? answerMap.get(term) : term);
+		}
+		return groundTerms;
 	}
 }
--- rulewerk-asp/src/test/java/org/semanticweb/rulewerk/asp/AspifGrounderTest.java
+++ rulewerk-asp/src/test/java/org/semanticweb/rulewerk/asp/AspifGrounderTest.java
@@ -21,7 +21,9 @@ package org.semanticweb.rulewerk.asp;
  */
 
 import org.junit.Test;
+import org.semanticweb.rulewerk.asp.implementation.AspReasonerImpl;
 import org.semanticweb.rulewerk.asp.implementation.AspifGrounder;
+import org.semanticweb.rulewerk.asp.model.AspReasoner;
 import org.semanticweb.rulewerk.asp.model.Grounder;
 import org.semanticweb.rulewerk.core.model.api.*;
 import org.semanticweb.rulewerk.core.model.implementation.Expressions;
@@ -32,11 +34,14 @@ import org.semanticweb.rulewerk.reasoner.vlog.VLogReasoner;
 import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.StringWriter;
+import java.util.Arrays;
 import java.util.Map;
 
 import static org.junit.Assert.assertEquals;
 
 public class AspifGrounderTest {
+
+	final Variable x = Expressions.makeUniversalVariable("X");
 
 	final Constant c = Expressions.makeAbstractConstant("c");
 	final Constant d = Expressions.makeAbstractConstant("d");
@@ -55,14 +60,68 @@ public class AspifGrounderTest {
 		grounder.ground();
 		bufferedWriter.flush();
 		assertEquals("asp 1 0 0\n" +
-			"4 1 1 1 1\n" +
 			"1 0 1 1 0 0\n" +
-			"4 1 2 1 2\n" +
 			"1 0 1 2 0 0\n" +
+			"4 1 2 1 2\n" +
+			"4 1 1 1 1\n" +
 			"0\n", writer.toString());
 		Map<Integer, Literal> map = grounder.getIntegerLiteralMap();
 		assertEquals(2, map.size());
 		assertEquals(fact, map.getOrDefault(1, null));
 		assertEquals(fact2, map.getOrDefault(2, null));
+	}
+
+	@Test
+	public void visitRuleTest() throws IOException {
+		KnowledgeBase knowledgeBase = new KnowledgeBase();
+		Fact fact = Expressions.makeFact("r", c);
+		Fact fact2 = Expressions.makeFact("r", d);
+		Fact fact3 = Expressions.makeFact("s", c);
+		Fact fact4 = Expressions.makeFact("s", d);
+		knowledgeBase.addStatements(fact, fact2, fact3, fact4);
+
+		Conjunction<PositiveLiteral> head = Expressions.makeConjunction(Arrays.asList(
+			Expressions.makePositiveLiteral("p", c, x, d),
+			Expressions.makePositiveLiteral("q", d)
+		));
+		Conjunction<Literal> body = Expressions.makeConjunction(Arrays.asList(
+			Expressions.makePositiveLiteral("r", x),
+			Expressions.makeNegativeLiteral("s", x)
+		));
+		knowledgeBase.addStatement(Expressions.makeRule(head, body));
+		AspReasoner aspReasoner = new AspReasonerImpl(knowledgeBase);
+		Reasoner reasoner = new VLogReasoner(aspReasoner.getDatalogKnowledgeBase());
+		StringWriter writer = new StringWriter();
+		BufferedWriter bufferedWriter = new BufferedWriter(writer);
+		Grounder grounder = new AspifGrounder(knowledgeBase, reasoner, bufferedWriter);
+		grounder.ground();
+		bufferedWriter.flush();
+
+		assertEquals("asp 1 0 0\n" +
+			"1 0 1 1 0 0\n" +
+			"1 0 1 2 0 0\n" +
+			"1 0 1 3 0 0\n" +
+			"1 0 1 4 0 0\n" +
+			"1 0 1 5 0 2 1 -3\n" +
+			"1 0 1 6 0 2 1 -3\n" +
+			"1 0 1 7 0 2 2 -4\n" +
+			"1 0 1 6 0 2 2 -4\n" +
+			"4 1 2 1 2\n" +
+			"4 1 6 1 6\n" +
+			"4 1 4 1 4\n" +
+			"4 1 1 1 1\n" +
+			"4 1 3 1 3\n" +
+			"4 1 7 1 7\n" +
+			"4 1 5 1 5\n" +
+			"0\n", writer.toString());
+		Map<Integer, Literal> map = grounder.getIntegerLiteralMap();
+		assertEquals(7, map.size());
+		assertEquals(fact, map.getOrDefault(1, null));
+		assertEquals(fact2, map.getOrDefault(2, null));
+		assertEquals(fact3, map.getOrDefault(3, null));
+		assertEquals(fact4, map.getOrDefault(4, null));
+		assertEquals(Expressions.makePositiveLiteral("p", c, c, d), map.getOrDefault(5, null));
+		assertEquals(Expressions.makePositiveLiteral("q", d), map.getOrDefault(6, null));
+		assertEquals(Expressions.makePositiveLiteral("p", c, d, d), map.getOrDefault(7, null));
 	}
 }
