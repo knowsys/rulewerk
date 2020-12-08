acd160f2b3db177daee4e429baf7adc7163bb0ad
Larry Gonzalez <larryjgonzalez@gmail.com>
2020-12-08 15:36:22 +0100

    delete unused imports

--- rulewerk-reliances/src/main/java/org/semanticweb/rulewerk/reliances/Reliance.java
+++ rulewerk-reliances/src/main/java/org/semanticweb/rulewerk/reliances/Reliance.java
@@ -1,10 +1,6 @@
 package org.semanticweb.rulewerk.reliances;
 
-import java.util.ArrayList;
-//import java.util.Arrays;
-import java.util.HashSet;
 import java.util.List;
-import java.util.Set;
 
 /*-
  * #%L
