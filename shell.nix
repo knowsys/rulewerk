5bc28147fad146ca056cfb5e08b3912d4f881661
Maximilian Marx <mmarx@wh2.tu-dresden.de>
2021-02-17 05:27:37 +0100

    Ensure target directory for local jvlog exists

--- shell.nix
+++ shell.nix
@@ -5,7 +5,8 @@ let pkgs = import <nixpkgs> {};
 	deps = dependencies.deps // { maven = maven; };
 in pkgs.mkShell {
 	buildInputsNative = [ maven deps.jdk dependencies.vlog ];
-	shellHook = ''
+	shellHook = '''
+	  mkdir -p rulewerk-vlog/lib/
 	  ln -sf ${dependencies.vlog.dev}/jvlog.jar rulewerk-vlog/lib/jvlog-local.jar
 	  mvn initialize -Pdevelopment
 	  mvn install -DskipTests
