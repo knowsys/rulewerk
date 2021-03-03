67fc952f1f85c02e9ea3409c8a74d5ff96a0386a
Larry Gonzalez <larryjgonzalez@gmail.com>
2021-03-03 17:03:34 +0100

    add tests for RuleUtil.isRuleApplicable

--- rulewerk-reliances/src/test/java/org/semanticweb/rulewerk/utils/RuleUtilTest.java
+++ rulewerk-reliances/src/test/java/org/semanticweb/rulewerk/utils/RuleUtilTest.java
@@ -20,6 +20,56 @@ package org.semanticweb.rulewerk.utils;
  * #L%
  */
 
-//TODO
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.assertFalse;
+
+import org.junit.Test;
+import org.semanticweb.rulewerk.core.model.api.Rule;
+import org.semanticweb.rulewerk.parser.ParsingException;
+import org.semanticweb.rulewerk.parser.RuleParser;
+
+//TODO add more tests
 public class RuleUtilTest {
+
+	@Test
+	public void isRuleApplicable_001() throws ParsingException {
+		Rule qy_px = RuleParser.parseRule("q(!Y) :- p(?X) .");
+
+		assertTrue(RuleUtil.isRuleApplicable(qy_px));
+	}
+
+	@Test
+	public void isRuleApplicable_002() throws ParsingException {
+		Rule qc_qc = RuleParser.parseRule("q(c) :- q(c) .");
+
+		assertFalse(RuleUtil.isRuleApplicable(qc_qc));
+	}
+
+	@Test
+	public void isRuleApplicable_003() throws ParsingException {
+		Rule qy_qc = RuleParser.parseRule("q(!Y) :- q(c) .");
+
+		assertFalse(RuleUtil.isRuleApplicable(qy_qc));
+	}
+
+	@Test
+	public void isRuleApplicable_004() throws ParsingException {
+		Rule qc_qx = RuleParser.parseRule("q(c) :- q(?X) .");
+
+		assertTrue(RuleUtil.isRuleApplicable(qc_qx));
+	}
+
+	@Test
+	public void isRuleApplicable_005() throws ParsingException {
+		Rule qy_qx = RuleParser.parseRule("q(!Y) :- q(?X) .");
+
+		assertFalse(RuleUtil.isRuleApplicable(qy_qx));
+	}
+
+	@Test
+	public void isRuleApplicable_006() throws ParsingException {
+		Rule qx_qx = RuleParser.parseRule("q(?X) :- q(?X) .");
+
+		assertFalse(RuleUtil.isRuleApplicable(qx_qx));
+	}
 }
