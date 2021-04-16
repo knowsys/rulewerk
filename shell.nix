ef91059abac7780ba9fd52b71c86232d99cb9a52
Maximilian Marx <mmarx@wh2.tu-dresden.de>
2021-04-16 21:03:22 +0200

    Fix typo in nix shell hook

--- shell.nix
+++ shell.nix
@@ -5,7 +5,7 @@ let pkgs = import <nixpkgs> {};
 	deps = dependencies.deps // { maven = maven; };
 in pkgs.mkShell {
 	buildInputsNative = [ maven deps.jdk dependencies.vlog ];
-	shellHook = '''
+	shellHook = ''
 	  mkdir -p rulewerk-vlog/lib/
 	  ln -sf ${dependencies.vlog.dev}/jvlog.jar rulewerk-vlog/lib/jvlog-local.jar
 	  mvn --no-transfer-progress initialize -Pdevelopment
