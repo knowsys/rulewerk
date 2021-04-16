c5239878b7aa90bb5bba44d2c833c01902ab7a5a
Maximilian Marx <mmarx@wh2.tu-dresden.de>
2021-04-16 21:03:16 +0200

    Update license headers

--- rulewerk-integrationtests/src/test/java/org/semanticweb/rulewerk/integrationtests/vlogissues/VLogIssue63IT.java
+++ rulewerk-integrationtests/src/test/java/org/semanticweb/rulewerk/integrationtests/vlogissues/VLogIssue63IT.java
@@ -1,5 +1,25 @@
 package org.semanticweb.rulewerk.integrationtests.vlogissues;
 
+/*-
+ * #%L
+ * Rulewerk Integration Tests
+ * %%
+ * Copyright (C) 2018 - 2021 Rulewerk Developers
+ * %%
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * #L%
+ */
+
 import java.io.IOException;
 
 import org.junit.Ignore;
@@ -9,7 +29,6 @@ import org.semanticweb.rulewerk.parser.ParsingException;
 
 public class VLogIssue63IT extends VLogIssue {
 
-	@Ignore
 	@Test
 	public void test() throws ParsingException, IOException {
 		try (final Reasoner reasoner = getReasonerWithKbFromResource("vlog/63.rls")) {
