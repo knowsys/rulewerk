2e171851d16b616fd6caa1c41b8ee7c5c76f210b
Larry Gonzalez <larryjgonzalez@gmail.com>
2021-05-27 15:18:10 +0200

    add tests

--- rulewerk-reliances/src/test/java/org/semanticweb/rulewerk/logic/MartelliMontanariUnifierTest.java
+++ rulewerk-reliances/src/test/java/org/semanticweb/rulewerk/logic/MartelliMontanariUnifierTest.java
@@ -23,125 +23,116 @@ package org.semanticweb.rulewerk.logic;
 import static org.junit.Assert.assertTrue;
 import static org.junit.Assert.assertFalse;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.HashMap;
+import java.util.Map;
 
 import org.junit.Test;
 import org.semanticweb.rulewerk.core.model.api.Literal;
-import org.semanticweb.rulewerk.math.mapping.PartialMapping;
 import org.semanticweb.rulewerk.parser.RuleParser;
 
 public class MartelliMontanariUnifierTest {
 
-	PartialMapping pm = new PartialMapping(new int[] { 0 }, 1);
-	List<Literal> l1 = new ArrayList<>();
-	List<Literal> l2 = new ArrayList<>();
+	Map<Literal, Literal> m = new HashMap<>();
+	MartelliMontanariUnifier unifier;
 
-	@Test
+	@Test(expected = IllegalArgumentException.class)
 	public void test01() throws Exception {
-		l1.clear();
-		l2.clear();
-		l1.add(RuleParser.parseLiteral("q(?X1,?X1)"));
-		l2.add(RuleParser.parseLiteral("q(!X2,!X2)"));
+		m.clear();
+		m.put(RuleParser.parseLiteral("q(?X1,?X1)"), RuleParser.parseLiteral("q(!X2,!X2)"));
 
-		MartelliMontanariUnifier unifier = new MartelliMontanariUnifier(l1, l2, pm);
-
-		assertTrue(unifier.getSuccess());
+		unifier = new MartelliMontanariUnifier(m);
+		assertTrue(unifier.isSuccessful());
 	}
 
 	@Test
 	public void test02() throws Exception {
-		l1.clear();
-		l2.clear();
-		l1.add(RuleParser.parseLiteral("q(?X1,?X1)"));
-		l2.add(RuleParser.parseLiteral("q(?X2,c)"));
+		m.clear();
+		m.put(RuleParser.parseLiteral("q(?X1,?X1)"), RuleParser.parseLiteral("q(?X2,c)"));
 
-		MartelliMontanariUnifier unifier = new MartelliMontanariUnifier(l1, l2, pm);
-
-		assertTrue(unifier.getSuccess());
+		unifier = new MartelliMontanariUnifier(m);
+		assertTrue(unifier.isSuccessful());
 	}
 
-	@Test
+	@Test(expected = IllegalArgumentException.class)
 	public void test03() throws Exception {
-		l1.clear();
-		l2.clear();
-		l1.add(RuleParser.parseLiteral("r(?X10001, !Y10001, !Z10001)"));
-		l2.add(RuleParser.parseLiteral("r(c, ?X20002, ?Y20002)"));
+		m.clear();
+		m.put(RuleParser.parseLiteral("r(?X10001, !Y10001, !Z10001)"),
+				RuleParser.parseLiteral("r(c, ?X20002, ?Y20002)"));
 
-		MartelliMontanariUnifier unifier = new MartelliMontanariUnifier(l1, l2, pm);
-
-		assertTrue(unifier.getSuccess());
+		unifier = new MartelliMontanariUnifier(m);
+		assertTrue(unifier.isSuccessful());
 	}
 
 	@Test
 	public void test04() throws Exception {
-		l1.clear();
-		l2.clear();
-		l1.add(RuleParser.parseLiteral("p(?X)"));
-		l2.add(RuleParser.parseLiteral("q(?X)"));
+		m.clear();
+		m.put(RuleParser.parseLiteral("p(?X)"), RuleParser.parseLiteral("q(?X)"));
 
-		MartelliMontanariUnifier unifier = new MartelliMontanariUnifier(l1, l2, pm);
-
-		assertFalse(unifier.getSuccess());
+		unifier = new MartelliMontanariUnifier(m);
+		assertFalse(unifier.isSuccessful());
 	}
 
 	@Test
 	public void test05() throws Exception {
-		l1.clear();
-		l2.clear();
-		l1.add(RuleParser.parseLiteral("p(?Y,?X)"));
-		l2.add(RuleParser.parseLiteral("p(?X,?Y)"));
+		m.clear();
+		m.put(RuleParser.parseLiteral("p(?Y,?X)"), RuleParser.parseLiteral("p(?X,?Y)"));
 
-		MartelliMontanariUnifier unifier = new MartelliMontanariUnifier(l1, l2, pm);
-
-		assertTrue(unifier.getSuccess());
+		unifier = new MartelliMontanariUnifier(m);
+		assertTrue(unifier.isSuccessful());
 	}
 
-	@Test
+	@Test(expected = IllegalArgumentException.class)
 	public void test06() throws Exception {
-		l1.clear();
-		l2.clear();
-		l1.add(RuleParser.parseLiteral("p(!Y,!X)"));
-		l2.add(RuleParser.parseLiteral("p(!X,!Y)"));
+		m.clear();
+		m.put(RuleParser.parseLiteral("p(!Y,!X)"), RuleParser.parseLiteral("p(!X,!Y)"));
 
-		MartelliMontanariUnifier unifier = new MartelliMontanariUnifier(l1, l2, pm);
-
-		assertTrue(unifier.getSuccess());
+		unifier = new MartelliMontanariUnifier(m);
+		assertTrue(unifier.isSuccessful());
 	}
 
 	@Test
 	public void test07() throws Exception {
-		l1.clear();
-		l2.clear();
-		l1.add(RuleParser.parseLiteral("p(?x1,?x1,?x1)"));
-		l2.add(RuleParser.parseLiteral("p(?x2,c1,c2)"));
+		m.clear();
+		m.put(RuleParser.parseLiteral("p(?x1,?x1,?x1)"), RuleParser.parseLiteral("p(?x2,c1,c2)"));
 
-		MartelliMontanariUnifier unifier = new MartelliMontanariUnifier(l1, l2, pm);
-
-		assertFalse(unifier.getSuccess());
+		unifier = new MartelliMontanariUnifier(m);
+		assertFalse(unifier.isSuccessful());
 	}
 
 	@Test
 	public void test08() throws Exception {
-		l1.clear();
-		l2.clear();
-		l1.add(RuleParser.parseLiteral("p(c)"));
-		l2.add(RuleParser.parseLiteral("p(c)"));
+		m.clear();
+		m.put(RuleParser.parseLiteral("p(c)"), RuleParser.parseLiteral("p(c)"));
 
-		MartelliMontanariUnifier unifier = new MartelliMontanariUnifier(l1, l2, pm);
-
-		assertTrue(unifier.getSuccess());
+		unifier = new MartelliMontanariUnifier(m);
+		assertTrue(unifier.isSuccessful());
 	}
 
 	@Test
 	public void test09() throws Exception {
-		l1.clear();
-		l2.clear();
-		l1.add(RuleParser.parseLiteral("p(c)"));
-		l2.add(RuleParser.parseLiteral("p(d)"));
+		m.clear();
+		m.put(RuleParser.parseLiteral("p(c)"), RuleParser.parseLiteral("p(d)"));
 
-		MartelliMontanariUnifier unifier = new MartelliMontanariUnifier(l1, l2, pm);
+		unifier = new MartelliMontanariUnifier(m);
+		assertFalse(unifier.isSuccessful());
+	}
+	
+	@Test
+	public void test10() throws Exception {
+		m.clear();
+		m.put(RuleParser.parseLiteral("p(?Y,?X)"), RuleParser.parseLiteral("p(?X,?Y)"));
 
-		assertFalse(unifier.getSuccess());
+		unifier = new MartelliMontanariUnifier(m);
+		assertTrue(unifier.isSuccessful());
+	}
+	
+	@Test
+	public void test11() throws Exception {
+		m.clear();
+		m.put(RuleParser.parseLiteral("r(?X1, ?Y1, ?Z1)"),
+				RuleParser.parseLiteral("r(c, ?X2, ?Y2)"));
+
+		unifier = new MartelliMontanariUnifier(m);
+		assertTrue(unifier.isSuccessful());
 	}
 }
--- rulewerk-reliances/src/test/java/org/semanticweb/rulewerk/reliances/RelianceTest.java
+++ rulewerk-reliances/src/test/java/org/semanticweb/rulewerk/reliances/RelianceTest.java
@@ -1,5 +1,3 @@
-package org.semanticweb.rulewerk.reliances;
-
 /*-
  * #%L
  * Rulewerk Reliances
@@ -9,9 +7,9 @@ package org.semanticweb.rulewerk.reliances;
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *      http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -20,57 +18,124 @@ package org.semanticweb.rulewerk.reliances;
  * #L%
  */
 
+package org.semanticweb.rulewerk.reliances;
+
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
 import org.junit.Test;
-
 import org.semanticweb.rulewerk.core.model.api.Rule;
 import org.semanticweb.rulewerk.parser.RuleParser;
 
 public class RelianceTest {
-
+	
 	@Test
-	public void simpleDatalogRuleTest() throws Exception {
-		Rule rule1 = RuleParser.parseRule("q(?X) :- p(?X) .");
-		Rule rule2 = RuleParser.parseRule("r(?X) :- q(?X) .");
+	public void noMatchingAtomsTest() throws Exception {
+		Rule px_qx = RuleParser.parseRule("q(?X) :- p(?X) .");
 
-		assertFalse(Reliance.positively(rule1, rule1));
-		assertTrue(Reliance.positively(rule1, rule2));
-		assertFalse(Reliance.positively(rule2, rule1));
-		assertFalse(Reliance.positively(rule2, rule2));
+		assertFalse(Reliance.positively(px_qx, px_qx));
 	}
 
 	@Test
-	public void cyclicDependency() throws Exception {
-		Rule rule1 = RuleParser.parseRule("q(?X) :- p(?X) .");
-		Rule rule2 = RuleParser.parseRule("p(?X) :- q(?X) .");
+	public void chainUni2UniTest() throws Exception {
+		Rule px_qx = RuleParser.parseRule("q(?X) :- p(?X) .");
+		Rule qx_rx = RuleParser.parseRule("r(?X) :- q(?X) .");
 
-		assertFalse(Reliance.positively(rule1, rule1));
-		assertFalse(Reliance.positively(rule1, rule2));
-		assertFalse(Reliance.positively(rule2, rule1));
-		assertFalse(Reliance.positively(rule2, rule2));
+		assertTrue(Reliance.positively(px_qx, qx_rx));
 	}
 
 	@Test
-	public void verySimpleExistentialRuleTest() throws Exception {
-		Rule rule1 = RuleParser.parseRule("q(!Y) :- p(?X) .");
-		Rule rule2 = RuleParser.parseRule("r(!Z) :- q(c) .");
-		assertFalse(Reliance.positively(rule1, rule1));
-		//assertFalse(Reliance.positively(rule1, rule2)); // THIS IS A BUG. IT SHOULD BE FALSE
-		assertFalse(Reliance.positively(rule2, rule1));
-		assertFalse(Reliance.positively(rule2, rule2));
+	public void chainUni2ExiTest() throws Exception {
+		Rule px_qy = RuleParser.parseRule("q(!Y) :- p(?X) .");
+		Rule qx_rx = RuleParser.parseRule("r(?X) :- q(?X) .");
+
+		assertTrue(Reliance.positively(px_qy, qx_rx));
 	}
 
 	@Test
-	public void simpleExistentialRuleTest() throws Exception {
-		Rule rule1 = RuleParser.parseRule("q(?X,!Y) :- p(?X) .");
-		Rule rule2 = RuleParser.parseRule("r(?X,?Y) :- q(?X,?Y) .");
+	public void chainUni2ConTest() throws Exception {
+		Rule px_qc = RuleParser.parseRule("q(c) :- p(?X) .");
+		Rule qx_rx = RuleParser.parseRule("r(?X) :- q(?X) .");
 
-		assertFalse(Reliance.positively(rule1, rule1));
-		assertTrue(Reliance.positively(rule1, rule2));
-		assertFalse(Reliance.positively(rule2, rule1));
-		assertFalse(Reliance.positively(rule2, rule2));
+		assertTrue(Reliance.positively(px_qc, qx_rx));
+	}
+
+	@Test
+	public void chainCon2UniTest() throws Exception {
+		Rule px_qx = RuleParser.parseRule("q(?X) :- p(?X) .");
+		Rule qc_rx = RuleParser.parseRule("r(!X) :- q(c) .");
+
+		assertTrue(Reliance.positively(px_qx, qc_rx));
+	}
+
+	@Test
+	public void chainCon2ExiTest() throws Exception {
+		Rule px_qy = RuleParser.parseRule("q(!Y) :- p(?X) .");
+		Rule qc_rx = RuleParser.parseRule("r(!X) :- q(c) .");
+
+		assertFalse(Reliance.positively(px_qy, qc_rx));
+	}
+
+	@Test
+	public void chainCon2ConTest() throws Exception {
+		Rule px_qc = RuleParser.parseRule("q(c) :- p(?X) .");
+		Rule qc_rx = RuleParser.parseRule("r(!X) :- q(c) .");
+		Rule qd_rx = RuleParser.parseRule("r(!X) :- q(d) .");
+
+		assertTrue(Reliance.positively(px_qc, qc_rx));
+		assertFalse(Reliance.positively(px_qc, qd_rx));
+	}
+
+	@Test
+	public void cyclicUni2UniTest() throws Exception {
+		Rule px_qx = RuleParser.parseRule("q(?X) :- p(?X) .");
+		Rule qx_px = RuleParser.parseRule("p(?X) :- q(?X) .");
+
+		assertFalse(Reliance.positively(px_qx, qx_px));
+	}
+
+	@Test
+	public void cyclicUni2ExiTest() throws Exception {
+		Rule px_qy = RuleParser.parseRule("q(!Y) :- p(?X) .");
+		Rule qx_px = RuleParser.parseRule("p(?X) :- q(?X) .");
+
+		assertFalse(Reliance.positively(px_qy, qx_px));
+		assertFalse(Reliance.positively(qx_px, px_qy));
+	}
+
+	@Test
+	public void cyclicUni2ConTest() throws Exception {
+		Rule px_qc = RuleParser.parseRule("q(c) :- p(?X) .");
+		Rule qx_px = RuleParser.parseRule("p(?X) :- q(?X) .");
+
+		assertTrue(Reliance.positively(px_qc, qx_px));
+		assertTrue(Reliance.positively(qx_px, px_qc));
+	}
+
+	@Test
+	public void cyclicCon2UniTest() throws Exception {
+		Rule px_qx = RuleParser.parseRule("q(?X) :- p(?X) .");
+		Rule qc_px = RuleParser.parseRule("p(!X) :- q(c) .");
+
+		assertFalse(Reliance.positively(px_qx, qc_px));
+	}
+
+	@Test
+	public void cyclicCon2ExiTest() throws Exception {
+		Rule px_qy = RuleParser.parseRule("q(!Y) :- p(?X) .");
+		Rule qc_px = RuleParser.parseRule("p(!X) :- q(c) .");
+
+		assertFalse(Reliance.positively(px_qy, qc_px));
+	}
+
+	@Test
+	public void cyclicCon2ConTest() throws Exception {
+		Rule px_qc = RuleParser.parseRule("q(c) :- p(?X) .");
+		Rule qc_px = RuleParser.parseRule("p(!X) :- q(c) .");
+		Rule qd_px = RuleParser.parseRule("p(!X) :- q(d) .");
+
+		assertFalse(Reliance.positively(px_qc, qc_px));
+		assertFalse(Reliance.positively(px_qc, qd_px));
 	}
 
 	@Test
@@ -133,7 +198,7 @@ public class RelianceTest {
 		Rule rule1 = RuleParser.parseRule("p(?Y,!Z) :- p(?X,?Y) .");
 		Rule rule2 = RuleParser.parseRule("p(?X,!Z) :- p(?X,?Y) .");
 
-		assertTrue(Reliance.positively(rule1, rule1));
+		assertFalse(Reliance.positively(rule1, rule1));
 		assertFalse(Reliance.positively(rule2, rule2));
 	}
 
@@ -159,11 +224,19 @@ public class RelianceTest {
 		assertTrue(Reliance.positively(rule2, rule2));
 	}
 
-	@Test
-	public void test05() throws Exception {
+	@Test(expected = IllegalArgumentException.class)
+	public void test05a() throws Exception {
 		Rule rule1 = RuleParser.parseRule("Q(?X,?Y) :- Q(?X,?Y) .");
 
-		assertFalse(Reliance.positively(rule1, rule1));
+		Reliance.positively(rule1, rule1);
+	}
+	
+	@Test(expected = IllegalArgumentException.class)
+	public void test05b() throws Exception {
+		Rule rule1 = RuleParser.parseRule("Q(?X,?Y) :- Q(?X,?Y) .");
+		Rule rule2 = RuleParser.parseRule("Q(?Y,?Y) :- Q(?X,?Y) .");
+
+		Reliance.positively(rule2, rule1);
 	}
 
 	@Test
@@ -173,7 +246,7 @@ public class RelianceTest {
 
 		assertFalse(Reliance.positively(rule1, rule1));
 		assertFalse(Reliance.positively(rule1, rule2));
-		assertTrue(Reliance.positively(rule2, rule1));
+		assertFalse(Reliance.positively(rule2, rule1));
 		assertFalse(Reliance.positively(rule2, rule2));
 	}
 
@@ -189,9 +262,9 @@ public class RelianceTest {
 		Rule rule1 = RuleParser.parseRule("P(?X,!U), P(!U,!V) :- P(?X,?Y) .");
 		Rule rule2 = RuleParser.parseRule("P(?X,!U), P(!U,!V) :- Q(?X,?Y) .");
 
-		assertTrue(Reliance.positively(rule1, rule1));
+		assertFalse(Reliance.positively(rule1, rule1));
 		assertFalse(Reliance.positively(rule1, rule2));
-		assertTrue(Reliance.positively(rule2, rule1));
+		assertFalse(Reliance.positively(rule2, rule1));
 		assertFalse(Reliance.positively(rule2, rule2));
 	}
 
@@ -214,21 +287,11 @@ public class RelianceTest {
 
 	@Test
 	public void test10() throws Exception {
-		Rule rule1 = RuleParser.parseRule("P(!U,?Z,?Y) :- P(?X,?Y,?Z) .");
-		Rule rule2 = RuleParser.parseRule("P(?Z,!U,?X) :- P(?X,?Y,?Z) .");
-		Rule rule3 = RuleParser.parseRule("P(?Y,?X,!U) :- P(?X,?Y,?Z) .");
+		Rule rule1 = RuleParser.parseRule("P(?Y,?X) :- P(?X,?Y) .");
 
 		assertFalse(Reliance.positively(rule1, rule1));
-		assertTrue(Reliance.positively(rule1, rule2));
-		assertTrue(Reliance.positively(rule1, rule3));
-		assertTrue(Reliance.positively(rule2, rule1));
-		assertFalse(Reliance.positively(rule2, rule2));
-		assertTrue(Reliance.positively(rule2, rule3));
-		assertTrue(Reliance.positively(rule3, rule1));
-		assertTrue(Reliance.positively(rule3, rule2));
-		assertFalse(Reliance.positively(rule3, rule3));
 	}
-
+	
 	@Test
 	public void test11() throws Exception {
 		Rule rule1 = RuleParser.parseRule("q(?X,!U,!V), q(?Y,!V,!U), q(?Z,!V,!W) :- q(?X,?Y,?Z) .");
@@ -260,4 +323,34 @@ public class RelianceTest {
 		assertFalse(Reliance.positively(rule2, rule1));
 		assertFalse(Reliance.positively(rule2, rule2));
 	}
+	
+	@Test
+	public void test15() throws Exception {
+		Rule rule1 = RuleParser.parseRule("P(!U,?Z,?Y) :- P(?X,?Y,?Z) .");
+		Rule rule2 = RuleParser.parseRule("P(?Z,!U,?X) :- P(?X,?Y,?Z) .");
+		Rule rule3 = RuleParser.parseRule("P(?Y,?X,!U) :- P(?X,?Y,?Z) .");
+
+		assertFalse(Reliance.positively(rule1, rule1));
+		assertTrue(Reliance.positively(rule1, rule2));
+		assertTrue(Reliance.positively(rule1, rule3));
+		assertTrue(Reliance.positively(rule2, rule1));
+		assertFalse(Reliance.positively(rule2, rule2));
+		assertTrue(Reliance.positively(rule2, rule3));
+		assertTrue(Reliance.positively(rule3, rule1));
+		assertTrue(Reliance.positively(rule3, rule2));
+		assertFalse(Reliance.positively(rule3, rule3));
+	}
+
+	@Test
+	public void rulesNotApplicable() throws Exception {
+		Rule rule1 = RuleParser.parseRule("P(!U,?Y) :- P(?X,?Y) .");
+		Rule rule2 = RuleParser.parseRule("P(?Y,?Y) :- P(?X,?Y) .");
+
+		assertFalse(Reliance.positively(rule1, rule1));
+		assertFalse(Reliance.positively(rule1, rule2));
+		assertFalse(Reliance.positively(rule2, rule1));
+		assertFalse(Reliance.positively(rule2, rule2));
+	}
+	
 }
+
