189e8ee1658c26ebf79a41aa0a57834a01300756
Irina Dragoste <irina.dragoste@tu-dresden.de>
2021-05-29 16:01:58 +0200

    added integration test for VLog issue 50

---
+++ rulewerk-integrationtests/src/test/java/org/semanticweb/rulewerk/integrationtests/vlogissues/VLogIssue50IT.java
@@ -0,0 +1,46 @@
+package org.semanticweb.rulewerk.integrationtests.vlogissues;
+
+/*-
+ * #%L
+ * Rulewerk Integration Tests
+ * %%
+ * Copyright (C) 2018 - 2021 Rulewerk Developers
+ * %%
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * #L%
+ */
+
+import static org.junit.Assert.assertTrue;
+
+import java.io.IOException;
+
+import org.junit.Test;
+import org.semanticweb.rulewerk.core.reasoner.Reasoner;
+import org.semanticweb.rulewerk.parser.ParsingException;
+
+/**
+ * https://github.com/karmaresearch/vlog/issues/50
+ * 
+ * @author Irina Dragoste
+ *
+ */
+public class VLogIssue50IT extends VLogIssue {
+
+	@Test
+	public void ruleset_succeeds_noCrash() throws ParsingException, IOException {
+		try (final Reasoner reasoner = this.getReasonerWithKbFromResource("vlog/50.rls")) {
+			final boolean finished = reasoner.reason();
+			assertTrue(finished);
+		}
+	}
+}
