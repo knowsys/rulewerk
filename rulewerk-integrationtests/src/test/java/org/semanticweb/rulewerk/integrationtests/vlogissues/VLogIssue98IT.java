6b0cbff1f013ae67c33c66c8df439ea3c7f4af7b
Irina Dragoste <irina.dragoste@tu-dresden.de>
2022-11-14 19:37:42 +0100

    fix integration test VLog issue #98

--- rulewerk-integrationtests/src/test/java/org/semanticweb/rulewerk/integrationtests/vlogissues/VLogIssue98IT.java
+++ rulewerk-integrationtests/src/test/java/org/semanticweb/rulewerk/integrationtests/vlogissues/VLogIssue98IT.java
@@ -21,16 +21,25 @@ package org.semanticweb.rulewerk.integrationtests.vlogissues;
  */
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
 
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
 import java.util.stream.Collectors;
 
 import org.junit.Test;
 import org.semanticweb.rulewerk.core.model.api.Fact;
+import org.semanticweb.rulewerk.core.model.api.Term;
+import org.semanticweb.rulewerk.core.model.implementation.AbstractConstantImpl;
+import org.semanticweb.rulewerk.core.reasoner.Correctness;
+import org.semanticweb.rulewerk.core.reasoner.QueryResultIterator;
 import org.semanticweb.rulewerk.core.reasoner.Reasoner;
 import org.semanticweb.rulewerk.parser.ParsingException;
+import org.semanticweb.rulewerk.parser.RuleParser;
 
 /**
  * https://github.com/karmaresearch/vlog/issues/98
@@ -51,4 +60,19 @@ public class VLogIssue98IT extends VLogIssue {
 			assertEquals(expectedInferences, inferences);
 		}
 	}
+
+	private void testCorrectness(final Reasoner reasoner) throws ParsingException {
+		// part_of_molar_crown(mc1) .
+		try (final QueryResultIterator answerQuery = reasoner
+				.answerQuery(RuleParser.parsePositiveLiteral("part_of_molar_crown(?x)"), true)) {
+			assertEquals(Correctness.SOUND_AND_COMPLETE, answerQuery.getCorrectness());
+			assertTrue(answerQuery.hasNext());
+			final List<Term> terms = answerQuery.next().getTerms();
+			final List<Term> expectedTerms = new ArrayList<>();
+			expectedTerms.add(new AbstractConstantImpl("mc1"));
+			assertEquals(expectedTerms, terms);
+			assertFalse(answerQuery.hasNext());
+		}
+	}
+
 }
