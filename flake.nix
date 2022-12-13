8d019e4b62c20ea6e4b9a86c2b24c47f3f2c6689
Maximilian Marx <mmarx@wh2.tu-dresden.de>
2022-12-13 18:10:41 +0100

    Prefer flake-utils-plus over flake-utils; bump inputs

--- flake.nix
+++ flake.nix
@@ -2,14 +2,11 @@
   description = "Rulewerk, a java toolkit for reasoning with existential rules";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-22.05";
-    flake-utils.url = "github:numtide/flake-utils";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-22.11";
+    utils.url = "github:gytis-ivaskevicius/flake-utils-plus";
+    gitignore.url = "github:hercules-ci/gitignore.nix";
     flake-compat = {
       url = "github:edolstra/flake-compat";
-      flake = false;
-    };
-    gitignoresrc = {
-      url = "github:hercules-ci/gitignore.nix";
       flake = false;
     };
   };
@@ -17,49 +14,61 @@
   outputs = {
     self,
     nixpkgs,
-    flake-utils,
-    flake-compat,
-    gitignoresrc,
+    utils,
+    gitignore,
     ...
   } @ inputs: let
     getJdk = pkgs: pkgs.jdk8_headless;
   in
-    {
-      overlays.default = import ./nix {inherit getJdk gitignoresrc;};
-    }
-    // (flake-utils.lib.eachDefaultSystem (system: let
-      pkgs = import nixpkgs {
-        inherit system;
-        overlays = [self.overlays.default];
+    utils.lib.mkFlake {
+      inherit self inputs;
+
+      overlays.default = import ./nix {
+        inherit getJdk;
+        inherit (gitignore.lib) gitignoreSource;
       };
-    in rec {
-      formatter = pkgs.alejandra;
-      packages = flake-utils.lib.flattenTree {
-        inherit (pkgs) kognac trident vlog rulewerk;
-        default = pkgs.rulewerk;
-      };
-      apps = rec {
-        rulewerk = flake-utils.lib.mkApp {drv = packages.rulewerk;};
-        default = rulewerk;
-      };
-      devShells.default = let
+
+      sharedOverlays = [self.overlays.default];
+
+      outputsBuilder = channels: let
+        pkgs = channels.nixpkgs;
         jdk = getJdk pkgs;
-      in
-        pkgs.mkShell {
-          buildInputs = [
-            jdk
-            (pkgs.maven.override {inherit jdk;})
-            pkgs.kognac
-            pkgs.trident
-            pkgs.sparsehash
-            pkgs.curl
-            pkgs.lz4
-            pkgs.zlib
-            pkgs.rulewerk-debug
-          ];
-          shellHook = ''
-            export "PATH=${pkgs.rulewerk-debug}/bin:$PATH"
-          '';
+        maven = pkgs.maven.override {inherit jdk;};
+      in rec {
+        formatter = pkgs.alejandra;
+
+        packages = rec {
+          inherit (pkgs) kognac trident vlog rulewerk;
+          default = rulewerk;
         };
-    }));
+
+        apps = rec {
+          rulewerk = utils.lib.mkApp {drv = packages.rulewerk;};
+          default = rulewerk;
+          mvn2nix = utils.lib.mkApp {
+            drv = pkgs.writeShellScriptBin "mvn2nix" ''
+              ${maven}/bin/mvn org.nixos.mvn2nix:mvn2nix-maven-plugin:mvn2nix
+            '';
+          };
+        };
+
+        devShells.default =
+          pkgs.mkShell {
+            buildInputs = [
+              jdk
+              maven
+              pkgs.kognac
+              pkgs.trident
+              pkgs.sparsehash
+              pkgs.curl
+              pkgs.lz4
+              pkgs.zlib
+              pkgs.rulewerk-debug
+            ];
+            shellHook = ''
+              export "PATH=${pkgs.rulewerk-debug}/bin:$PATH"
+            '';
+          };
+      };
+    };
 }
